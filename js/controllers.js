667b6c66e81514dbbcfdd146404a900f4b42b7c5
Jasson Casey <jasson@flowgrammable.com>
2014-05-01 11:44:21 -0400

    Adding: more framework reuqirements

---
+++ js/controllers.js
@@ -0,0 +1,31 @@
+
+var flowsim = angular.module('flowsim', ['ngRoute', 'ui.bootstrap']);
+
+flowsim.controller('signinCtrlr', function($scope) {
+  console.log('singin-controller');
+});
+
+flowsim.controller('signupCtrlr', function($scope) {
+  console.log('singup-controller');
+});
+
+flowsim.controller('mainCtrlr', function($scope) {
+  console.log('main-controller');
+});
+
+flowsim.config(['$routeProvider', function($routeProvider) {
+  console.log('config');
+  $routeProvider.
+  when('/signin', {
+    templateUrl: 'signin.html',
+    controller: 'signinCtrlr'
+  }).
+  when('/signup', {
+    templateUrl: 'signup.html',
+    controller: 'signupCtlr'
+  }).
+  otherwise({
+    templateURL: 'main.html',
+    controller: 'mainCtrlr'
+  });
+}]);
