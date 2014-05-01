1281f2084aa930d25bf208d83de2741b0e1b39ed
Jasson Casey <jasson@flowgrammable.com>
2014-05-01 09:21:47 -0400

    Adding: initial stylings

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ function hello(req, res, next) {
 }
 
 connect()
+  .use(connect.favicon('img/favicon.png'))
   .use('/css', connect.static('bower_components/bootstrap/dist/css'))
   .use('/css', connect.static('bower_components/bootstrap/dist/fonts'))
   .use('/js', connect.static('bower_components/jquery/dist'))
   .use('/js', connect.static('bower_components/bootstrap/dist/js'))
+  .use('/img', connect.static('img'))
   .use('/css', connect.static('css'))
   .use('/js', connect.static('js'))
   .use(connect.static('html'))
