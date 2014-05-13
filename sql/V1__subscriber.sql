0b5bbeecfa591239bc85c4b9ad974244cda376b8
colton <coltonchojnacki@gmail.com>
2014-05-13 14:20:39 -0500

    sql update

--- sql/V1__subscriber.sql
+++ sql/V1__subscriber.sql
@@ -37,3 +37,20 @@ CREATE TABLE session
   status SESSION_STATUS NOT NULL                      -- current session status
 );
 
+
+CREATE TYPE version AS ENUM ('1.0', '1.1');
+
+-- create a profile
+CREATE TABLE profile
+(
+  id SERIAL PRIMARY KEY,
+  name            CHAR(128) NOT NULL ,
+  switch_version  version, 
+  no_ports        INTEGER NOT NULL,
+  no_tables       INTEGER NOT NULL,
+  table_size      INTEGER NOT NULL,
+  match_caps      JSON NOT NULL,
+  action_caps     JSON NOT NULL
+);
+  
+
