c9272df894253df9ee11ba2f69c40239cae93f5f
colton <coltonchojnacki@gmail.com>
2014-05-22 03:09:56 -0500

    mailer stuff

--- scratch/mailer/mailer.js
+++ scratch/mailer/mailer.js
@@ -4,10 +4,11 @@ module.exports = function(config,mailOptions, next){
 	var smtpTransport = nodemailer.createTransport("SMTP", config);
 
 	smtpTransport.sendMail(mailOptions, function(err, response){
-		if(err){
-			next(err);
-		}else{
-			console.log("Message sent: " + response.message);
+	    if(err){
+		smtpTransport.close();
+		next(err.name);
+	    }else{
+		console.log("Message sent: " + response.message);
 		}
 		smtpTransport.close();
 	});
