9d280c2762a7554258fb65d3991a9c7df28b4530
ojh302846 <oron.hazi@gmail.com>
2014-05-28 09:19:17 -0500

    Nodejs Exercises

---
+++ scratch/oron_scratch/nodejs/requestHandlers.js
@@ -0,0 +1,12 @@
+function start() {
+	console.log("Request handler 'start' was called.");
+	return "Hello Start";
+}
+
+function upload() {
+	console.log("Request handler 'upload' was called");
+	return "Hello Upload";
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ scratch/oron_scratch/nodejs/server.js
@@ -0,0 +1,41 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		var content = route(handle, pathname);
+		response.write(content);
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
