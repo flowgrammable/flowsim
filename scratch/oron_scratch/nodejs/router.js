2f8901fe7960656d4f97224219e5cbdd2b4b874b
ojh302846 <oron.hazi@gmail.com>
2014-05-28 15:36:49 -0500

    Nodejs exercise update

--- scratch/oron_scratch/nodejs/router.js
+++ scratch/oron_scratch/nodejs/router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response, request) {
 	console.log("About to route a request for " + pathname);
 	if(typeof handle[pathname] === 'function') {
-		return handle[pathname]();
+		handle[pathname](response, request);
 	}	else {
 		console.log("No request handler found for " + pathname);
-		return "404 Not Found";
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 Not found");
+		response.end();
 	}
 }
 
--- scratch/oron_scratch/nodejs/server.js
+++ scratch/oron_scratch/nodejs/server.js
@@ -5,11 +5,7 @@ function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		var content = route(handle, pathname);
-		response.write(content);
-		response.end();
+		route(handle, pathname, response, request);
 	}
 
 	http.createServer(onRequest).listen(8888);
