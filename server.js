79209542ef55a79ccd6e69e5e671aa66e39b183a
colton <coltonchojnacki@gmail.com>
2014-06-01 16:43:10 -0500

    fix

---
+++ server.js
@@ -0,0 +1,16 @@
+var restify = require('restify');
+var orm = require('orm');
+var environment = require('./conf/environment.js');
+var routes = require('./conf/routes');
+
+var server = restify.createServer();
+
+environment(server);
+routes(server);
+
+server.get('/', function(req, res, next){
+	console.log(req);
+	res.end('my response');
+	});
+
+server.listen(8000);
