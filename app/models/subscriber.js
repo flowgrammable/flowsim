9dc41eb418bc84e76d3cb02dc341ddc591eba800
ashxjain <ashxjain@github.com>
2014-06-05 02:54:22 -0500

    Update Sign-Up code

--- app/models/subscriber.js
+++ app/models/subscriber.js
@@ -3,47 +3,43 @@ var validator = require('validator');
 
 module.exports = function (db,orm) {
   var Subscriber = db.define('subscriber', {
-	id : { type: 'integer', unique: true, defaultValue: undefined },
-	email : { type: 'text', size: 128, unique: true },
-	password : { type: 'text', size: 60 },
-	reg_date : { type: 'date', time: true },
-	reg_ip : { type: 'text', size: 128 },
-	status : {
-		type: 'enum',
-		values : [ 'REGISTERED', 'VERIFIED'],
-		defaultValue: 'REGISTERED'
-	}
-/*	status_date : { type: 'date', time: true },
-	reg_key : { type: 'text', size: 64 }
+    id : { type: 'integer', unique: true, defaultValue: undefined },
+    email : { type: 'text', size: 128, unique: true },
+    password : { type: 'text', size: 60 },
+    reg_date : { type: 'date', time: true },
+    reg_ip : { type: 'text', size: 128 },
+    status : {
+        type: 'enum',
+        values : [ 'REGISTERED', 'VERIFIED'],
+        defaultValue: 'REGISTERED'
+    }
+/*  status_date : { type: 'date', time: true },
+    reg_key : { type: 'text', size: 64 }
 */
-	}, {
+    }, {
 
-	// validations for model	   
-	validations : {
-		password : orm.enforce.ranges.length(8,16, "invalid length")
-		// need to move validator.isEmail(email) here
-	}, 
-	
-	hooks : {
-		beforeSave: function (next) {
-		   var user = this;
-		   
-		   if (validator.isEmail(user.email)) {
-			// hash password
-			bcrypt.genSalt(10, function(err, salt) {
-			if(err) throw err;
-			    bcrypt.hash(user.password, salt, function(err, hash){
-				if(err) return next(err);
-				user.password = hash;
-				next();
-			    });
-			});
-			
-		   } else {
-		        return next(new Error("invalid email"));
-		   }
-		}
-	    }
-	});
+    // validations for model       
+    validations : {
+        password : orm.enforce.ranges.length(8,16, "Password is not between 8-16 chars"),
+        email : orm.enforce.patterns.email("Invalid Email")
+      
+    }, 
+
+    hooks : {
+        beforeSave: function (next) {
+           var user = this;
+            // hash password
+            bcrypt.genSalt(10, function(err, salt) {
+                if(err) throw err;
+                bcrypt.hash(user.password, salt, function(err, hash){
+                    if(err) return next(err);
+                    user.password = hash;
+                    next();
+                });
+            });
+
+        }
+        }
+    });
 
 }
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var restify = require('restify');
 var orm = require('orm');
 var environment = require('./conf/environment.js');
 var routes = require('./conf/routes');
+var request =  require('request');//For making HTTP requests in node JS
 
 var server = restify.createServer();
 
-environment(server);
-routes(server);
+environment(server);//Setting up the environment, passing server object.
+routes(server);//Setting up routes for CRUD http methods.
+
 
 server.get('/', function(req, res, next){
 	console.log(req);
@@ -14,3 +16,37 @@ server.get('/', function(req, res, next){
 	});
 
 server.listen(8000);
+//Invalid Email address
+request({
+  url: "http://localhost:8000/subscribers",
+  body: "{ \"email\": \"user3user3.com\", \"password1\": \"my password\", \"password2\":\"my password\", \"ip\":\"83.72.61.50\" }",
+  headers: {"Content-Type": "application/json"},
+  method: "POST"
+}, function (error, response, body) {
+  console.log("Status : "+ response.statusCode);
+//  console.log("Headers", JSON.stringify(response.headers));
+  console.log("Response received : ", body);
+});
+//Email already exists
+request({
+  url: "http://localhost:8000/subscribers",
+  body: "{ \"email\": \"user3@user3.com\", \"password1\": \"my password\", \"password2\":\"my password\", \"ip\":\"83.72.61.50\" }",
+  headers: {"Content-Type": "application/json"},
+  method: "POST"
+}, function (error, response, body) {
+  console.log("Status : "+ response.statusCode);
+//  console.log("Headers", JSON.stringify(response.headers));
+  console.log("Response received : ", body);
+});
+//Password length error
+request({
+  url: "http://localhost:8000/subscribers",
+  body: "{ \"email\": \"user4@user3.com\", \"password1\": \"my\", \"password2\":\"my password\", \"ip\":\"83.72.61.50\" }",
+  headers: {"Content-Type": "application/json"},
+  method: "POST"
+}, function (error, response, body) {
+  console.log("Status : "+ response.statusCode);
+//  console.log("Headers", JSON.stringify(response.headers));
+  console.log("Response received : ", body);
+});
+
