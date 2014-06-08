ff7583d95b67a97f13319f3ceac414c4de2c4ccb
colton <coltonchojnacki@gmail.com>
2014-06-08 12:15:30 -0500

    serving static

--- conf/environment.js
+++ conf/environment.js
@@ -1,9 +1,15 @@
 var database = require('./database.js');
 var restify = require('restify');
+var connect = require('connect');
+var settings = require('./settings');
 
 module.exports = function(server) {
 	server.use(require('connect').bodyParser());
-
+	server.get('/js',  restify.serveStatic({directory: 'public/js'}));
+	server.get('/img', restify.serveStatic({directory: 'public/img'}));
+	server.get('/css', restify.serveStatic({directory: 'public', default: 'css/style.css'}));
+	server.get('/', restify.serveStatic({directory: 'public',
+				default: 'html/index.html'}));
 	server.use(function(req, res, next){
 		database(function (err, db) {
 			if(err) return next(err);
