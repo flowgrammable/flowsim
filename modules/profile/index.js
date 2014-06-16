3dffe5bf923cb4c61264661093b4a4a777996d8a
colton <coltonchojnacki@gmail.com>
2014-06-16 15:43:53 -0500

    laying out auth example

--- modules/profile/index.js
+++ modules/profile/index.js
@@ -3,7 +3,8 @@ var profile = require('./controller.js');
 module.exports = function(server) {
 //	server.get('/profile', profile.list);
 	server.post('/profile', profile.create);
-//  server.get('/profile/:id', profile.read);
+//  server.post('/profile', passport.authenticate('bearer'), profile.create);
+//  server.get('/profile/:id', passport.authenticate('bearer'), profile.read);
 
 
 }
