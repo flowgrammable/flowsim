8dd0bf0f8d0f82f67590f73a91541e404d1a74af
colton <coltonchojnacki@gmail.com>
2014-06-17 17:34:52 -0500

    adding signin page

--- public/subscriber/controller.js
+++ public/subscriber/controller.js
@@ -1,6 +1,7 @@
 flowsim.controller('signupController', function($scope, $http){
     console.log('signup-controller');
     // do signup stuff here
+    $scope.hideSignup = false;
     $scope.subscriber = {};
     $scope.createSubscriber = function() {
 			$http({
@@ -8,13 +9,28 @@ flowsim.controller('signupController', function($scope, $http){
 					url : '/subscribers',
 					data : $scope.subscriber
 			}).success(function(data, status, headers, config){
-				$scope.data = data;
+				$scope.data = 'Registration Succesful - Check email for verification';
+        $scope.hideSignup = true;
+        
 			}).error(function(data, status, headers, config){
 				$scope.data = data;
 			})
 		}
 });
 
-flowsim.controller('signinController', function($scope, $http){
+flowsim.controller('signinController', function($scope, $http, $window){
     console.log('signin-controller');
+		$scope.subscriber = {};
+    $scope.loginSubscriber = function () {
+			$http
+				.post('/login', $scope.subscriber)
+				.sucess(function (data, status, headers, config) {
+					$window.sessionStorage.token = data.token;
+				})
+				.error(function (data, status, headers, config) {
+					delete $window.sessionStorage.token;
+
+					$scope.message = 'Error: Invalid user or password';
+				});
+		}
 });
