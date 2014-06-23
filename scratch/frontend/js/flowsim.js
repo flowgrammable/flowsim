92d869b050bb4faad247bfe3845973bd8737b137
Jasson Casey <jasson@flowgrammable.com>
2014-06-23 14:39:04 -0400

    Adding: flowsim

---
+++ scratch/frontend/js/flowsim.js
@@ -0,0 +1,42 @@
+
+var flowsimApp = angular.module('flowsimApp', ['ngRoute']);
+
+flowsimApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'main.html'
+    }).
+    when('/openflow', {
+      templateUrl: 'openflow.html'
+    }).
+    when('/about', {
+      templateUrl: 'about.html'
+    }).
+    when('/profile', {
+      templateUrl: 'profile.html'
+    }).
+    when('/packet', {
+      templateUrl: 'packet.html'
+    }).
+    when('/trace', {
+      templateUrl: 'trace.html'
+    }).
+    when('/switch', {
+      templateUrl: 'switch.html'
+    }).
+    when('/simulation', {
+      templateUrl: 'simulation.html'
+    }).
+    when('/register', {
+      templateUrl: 'register.html'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+}]);
+
+flowsimApp.controller('flowsimCtrl', function($scope) {
+  $scope.authenticated = false;
+});
+
+
