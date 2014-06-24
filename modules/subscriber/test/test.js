d4971a7a83eade9f9ef4ea47c3ee5763e5e722d1
ashxjain <ashxjain@github.com>
2014-06-24 09:05:14 -0500

    Updating subscriber module

--- modules/subscriber/test/test.js
+++ modules/subscriber/test/test.js
@@ -100,3 +100,67 @@ describe('Testing client requests:',function() {
     });
   });
 });
+
+describe('Testing client login:',function() {
+  it('Token gen successfully',function(done) {
+    request( {
+      url: 'http://localhost:8000/login',
+      body: '{ \"email\": \"'+testEmail+'\", \"password\":\"my password\" }',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log('\n\tStatus : '+ response.statusCode);
+      assert.equal(response.statusCode,200);
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
+  it('User with this email does not exists',function(done) {
+    request( {
+      url: 'http://localhost:8000/login',
+      body: '{ \"email\": \"Invalid@email.com\", \"password\":\"my password\" }',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log('\n\tStatus : '+ response.statusCode);
+      assert.equal(response.statusCode,401);
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
+  it('Password mismatch',function(done) {
+    request( {
+      url: 'http://localhost:8000/login',
+      body: '{ \"email\": \"'+testEmail+'\", \"password\":\"my password123\" }',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log('\n\tStatus : '+ response.statusCode);
+      assert.equal(response.statusCode,401);
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
+  it('Password mismatch',function(done) {
+    request( {
+      url: 'http://localhost:8000/login',
+      body: '{ \"email\": \"coltonchojnacki@gmail.com\", \"password\":\"my password\" }',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log('\n\tStatus : '+ response.statusCode);
+      assert.equal(response.statusCode,401);
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
+});
+
