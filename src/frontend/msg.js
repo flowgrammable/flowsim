cf2da96b42919252839dd53128f906018d8dbb90
Jasson Casey <jasson@flowgrammable.com>
2014-06-27 12:00:44 -0400

    latest

---
+++ src/frontend/msg.js
@@ -0,0 +1,13 @@
+
+exports.success = function(data) {
+  return {
+    success: data
+  }
+}
+
+exports.error = function(data) {
+  return {
+    error: data
+  }
+}
+
--- src/frontend/rest.js
+++ src/frontend/rest.js
@@ -1,123 +1,7 @@
 
 var url = require('url');
-
-var db = {
-  subscribers: [
-    {
-      subscriberId: 1,
-      name: "Jasson Casey",
-      email: "jasson.casey@gmail.com",
-      password: "iluvflowg"
-    }
-  ],
-  sessions: [
-    {
-      sessionId: '123456',
-      accessToken: 'asdfasdf',
-      subscriberId: 2
-    }
-  ]
-}
-
-function subscriber_lookup(email) {
-  var i;
-  for(i = 0; i < db.subscribers.length; ++i) {
-    if(db.subscribers[i].email == email)
-      return db.subscribers[i];
-  }
-  return {};
-}
-
-function lookupAccessToken(token) {
-  var i;
-  for(var i=0; i<db.sessions.length; ++i) {
-    if(db.sessions[i].accessToken = accessToken)
-      return db.session[i];
-  }
-  return null;
-}
-
-function login(req, res, next) {
-  var result;
-  if(!req.body.email || !req.body.password) {
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-      error: {
-      }
-    }));
-    return;
-  }
-  
-  result = subscriber_lookup(req.body.email);
-  if(!result.password || req.body.password != result.password) {
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-      error: {
-      }
-    }));
-    return;
-  }
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'X-Access-Token': 'onebigfuckingstringz'
-  });
-  res.end(JSON.stringify({
-    success: {
-    }
-  }));
-}
-
-// login, login, register, verify, reset
-
-var restModules = {
-  subscriber : {
-    noauth : {
-      register : function(method, params, data) {
-        return {
-          success : {}
-        };
-      },
-      verify : function(method, params, data) {
-        return {
-          success : {}
-        };
-      },
-      reset : function(method, params, data) {
-        return {
-          success : {}
-        };
-      },
-      login : function(method, params, data) {
-        return {
-          success : {}
-        };
-      }
-    },
-    auth : {
-      logout : function(session, method, params, data) {
-        return {
-          success : {}
-        };
-      }
-    }
-  }
-}
-
-function success(result) {
-  return {
-    success: result
-  };
-}
-
-function error(result) {
-  return {
-    error: result
-  };
-}
+var sub = require('./subscriber');
+var msg = require('./msg');
 
 function wrapRes(res, result) {
   res.writeHead('200', {
@@ -126,26 +10,22 @@ function wrapRes(res, result) {
   res.end(JSON.stringify(result));
 }
 
-module.exports = function(userDefinedModules) {
+module.exports = function(userModules) {
+  userModules.subscriber = sub.module;
   return function(req, res, next) {
     var result;
-
-    // grab the access token if it exists
-    var accessToken = '';
-    var session = null;
-    if(req.headers['X-Access-Token']) {
-      accessToken = req.headers['X-Access-Token'];
-      session = lookupAccessToken(accessToken);
-    }
 
     // respond to a bad url pathname
     var path = url.parse(req.url).pathname.split('/');
     if(path.length < 2) {
-      wrapRes(res, error({
+      wrapRes(res, msg.error({
         description: 'Service not identified'
       });
       return;
     }
+    
+    // grab the access token if it exists
+    var session = subscriber.getSession(req.headers);
 
     if(restModules[path[0]]) {
       var params = path.slice(2);
@@ -160,15 +40,16 @@ module.exports = function(userDefinedModules) {
         wrapRes(res, result);
         return;
       } else {
-        wrapRes(res, error({
+        wrapRes(res, msg.error({
           description: 'Service not found'
         });
         return
       }
     } else {
-      wrapRes(res, error({
+      wrapRes(res, msg.error({
         description: 'Module not found'
       });
     }
   }
 }
+
