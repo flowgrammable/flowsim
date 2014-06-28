e1d79f766174b054889691105e05e1c5acee69bb
Jasson Casey <jasson@flowgrammable.com>
2014-06-28 09:21:54 -0400

    Adding comments

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,16 +1,28 @@
 
+/* config.js - a simple object describing the layout of static files to serve
+ *
+ *  favicon - location of the site's favicon
+ *  html    - html document roots
+ *  css     - css document roots
+ *  fonts   - fonts document roots
+ *  img     - img document roots
+ *  js      - javascript document roots
+ */
+
 module.exports = {
   favicon: 'img/favicon.png',
-  fonts: [ 'bower_components/bootstrap/dist/fonts' ],
-  css: [ 'bower_components/bootstrap/dist/css', '../frontend/css' ],
-  img: [ 'img' ],
   html: [ 'html' ],
-  js: [ 'bower_components/bootstrap/dist/js', 
+  css: [ 'css',
+    'bower_components/bootstrap/dist/css'
+  ],
+  fonts: [ 'bower_components/bootstrap/dist/fonts' ],
+  img: [ 'img' ],
+  js: [ 'js',
+    'bower_components/bootstrap/dist/js', 
     'bower_components/jquery/dist', 
     'bower_components/angular',
     'bower_components/angular-route',
-    'bower_components/angular-ui-bootstrap/dist',
-    'js'
+    'bower_components/angular-ui-bootstrap/dist'
   ]
 };
 
