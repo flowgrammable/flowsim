cd0524e897fc1a85c0f452fce29975074694be4c
Colton Chojnacki <coltonchojnacki@gmail.com>
2014-07-07 08:38:50 -0500

    update

--- scratch/colton/callbacks/rest/controller.js
+++ scratch/colton/callbacks/rest/controller.js
@@ -7,15 +7,21 @@ var subscriber = require('./subscriber');
 module.exports = 
 function(req, res, next){ 
 		var path = url.parse(req.url).pathname.split('/');
-		var data = 'test data';
         var params = path.slice(2);
 
-
-        // Call async subscriber module
-        // once finished, unwrap async result, and respond to client
-        subscriber.handleSubscriber(req.method, params, req.body, 
-            function(result){ 
-                console.log('got a result: ', result );
-                res.end(result);
-            });
+        // dispatch module by path
+        // need to add checks to see module contains auth and noauth
+        switch(path[1]){
+            case 'subscriber':
+                 // call async subscriber module
+                 // once finished, unwrap result, and respond to client
+                 subscriber.handleSubscriber(req.method, params, req.body, 
+                    function(result){ 
+                        console.log('got a result: ', result );
+                        res.end(result);
+                    });
+            break;
+            default:
+            res.end('module does not exist');
+       }
 }
