3246574fa6456f002c639b037be92de765163b5f
ojh302846 <oron.hazi@gmail.com>
2014-07-15 08:48:45 -0500

    Update controllerUtils.js

--- src/backend/rest/subscriber/controllerUtils.js
+++ src/backend/rest/subscriber/controllerUtils.js
@@ -3,10 +3,11 @@
 
 function validEmail(email){
 	//use node-enfore library
+	enforce.patterns.email([email])
 }
 
 function validPassword(password){
-
+	enforce.security.password([[ luns816, ]password ])
 }
 
 exports.validPassword = validPassword;
