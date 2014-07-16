4e852084e4e6c9b92a6d5c489863db07e37e52e7
colton <coltonchojnacki@gmail.com>
2014-07-16 18:57:17 -0500

    rename

--- src/backend/rest/subscriber/test/modelTest.js
+++ src/backend/rest/subscriber/test/modelTest.js
@@ -4,7 +4,7 @@ var testAdapter = require('./testAdapter.js');
 var model = subModel(testAdapter);
 
 
-model.subscriber.create( 'test@test.com', 'mypassword', '127.0.0.1', function(result){
+model.subscriber.create('test@test.com', 'mypassword', '127.0.0.1', function(result){
 	console.log(result);
 });
 
