ab1b6b4db6eb93bb5e5f1ed0abb4d72d22d98243
colton <coltonchojnacki@gmail.com>
2014-07-19 15:22:13 -0500

    updated resttest

--- src/backend/rest/subscriber/test/restTest.js
+++ src/backend/rest/subscriber/test/restTest.js
@@ -11,9 +11,10 @@ var token;
 
 describe('Testing registration requests:',function() {
   it('User registered successfully',function(done) {
+    this.timeout(5000);
     request( {
       url: 'http://localhost:3000/api/subscriber/register',
-      body: '{ \"email\": \"'+testEmail+'\", \"password\": \"my password\", \"ip\": \"192.162.0.8\"}',
+      body: '{ \"email\": \"'+testEmail+'\", \"password\": \"my password\"}',
       headers: {
         'Content-Type': 'application/json'
       },
