76ce5e878747f804e2e975745c1183ec4cf218a5
ojh302846 <oron.hazi@gmail.com>
2014-07-21 13:39:24 -0500

    Update controllerUtils.js

--- src/backend/rest/subscriber/controllerUtils.js
+++ src/backend/rest/subscriber/controllerUtils.js
@@ -9,7 +9,8 @@ function invalidEmail(email){
 function invalidPassword(password){
 	var min = 8;
 	var max = 16;
-	return !validator.isLength(password, min, max);	
+	//return !validator.isLength(password, min, max);
+	return !(password.length<=max&&password.length>=min);
 }
 //function validToken(token){
 //	checks.add("testToken", enforce.ranges.length(36, 36, "bad token"))
