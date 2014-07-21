a5fea7c39e9bd0fe9cdb7dde9a081945a3d5bb58
ojh302846 <oron.hazi@gmail.com>
2014-07-21 13:15:12 -0500

    Update controllerUtils.js

--- src/backend/rest/subscriber/controllerUtils.js
+++ src/backend/rest/subscriber/controllerUtils.js
@@ -1,27 +1,14 @@
 
-var enforce = require('enforce');
+var validator = require('vallidator');
 
 function invalidEmail(email){
 	//use node-enfore library
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    	return !(re.test(email));
+	return !validator.isEmail(email);
 }
 
 function invalidPassword(password){
-	var checks = new enforce.Enforce();
-	checks.add("testPass", enforce.ranges.length(8, 16, "bad password"));
-	checks.check({
-		testPass : password
-	}, function (err) {
-		if(err){
-			return true;
-		}
-		else{
-			return false;
-		}
-	});
+	return !validator.isLength(password, 8, 16);	
 }
-
 //function validToken(token){
 //	checks.add("testToken", enforce.ranges.length(36, 36, "bad token"))
 //	checks.check({
