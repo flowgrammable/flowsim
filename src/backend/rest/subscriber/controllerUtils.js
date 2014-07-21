7013c4835534164abc70b72db061497aeece6313
ojh302846 <oron.hazi@gmail.com>
2014-07-21 00:07:22 -0500

    Update controllerUtils.js

--- src/backend/rest/subscriber/controllerUtils.js
+++ src/backend/rest/subscriber/controllerUtils.js
@@ -1,17 +1,32 @@
 
 var enforce = require('enforce');
-
+var checks = new enforce.Enforce();
 function validEmail(email){
 	//use node-enfore library
-	return enforce.patterns.email(email);
+	checks.add("testEmail", enforce.patterns.email("bad email"))
+	checks.check({
+		testEmail : email
+	}, function (err) {
+		console.log(err);
+	});
 }
 
 function validPassword(password){
-	return enforce.range.length(8, 16, password);
+	checks.add("testPass", enforce.range.length(8, 16, "bad password"))
+	checks.check({
+		testPass : password
+	}, function (err) {
+		console.log(err);
+	});
 }
 
 function validToken(token){
-	return enforce.ranges.length(36, 36, token);
+	checks.add("testToken", enforce.range.length(36, 36, "bad token"))
+	checks.check({
+		testToken : token
+	}, function (err) {
+		console.log(err);
+	});
 }
 
 exports.validPassword = validPassword;
