7ee45132ef66e03ddc0d3748801200a83e354fc3
ojh302846 <oron.hazi@gmail.com>
2014-07-21 10:29:35 -0500

    Update utilsTest.js

--- src/backend/rest/subscriber/test/utilsTest.js
+++ src/backend/rest/subscriber/test/utilsTest.js
@@ -6,19 +6,19 @@ var assert = require('assert');
 //invalid email returns false
 var badEmail = "ojh12345@gmail";
 var goodEmail = "ojh12345@yahoo.com";
-utils.validEmail(badEmail);
-utils.validEmail(goodEmail);
-//assert.equal(utils.validEmail(badEmail), false, ['invalid email']);
-//assert.equal(utils.validEmail(goodEmail), true, ['valid email']);
+//utils.validEmail(badEmail);
+//utils.validEmail(goodEmail);
+assert.equal(utils.validEmail(badEmail), true, ['invalid email']);
+assert.equal(utils.validEmail(goodEmail), false, ['valid email']);
 
 //password test
 //invalid password return false, needs to be 8-16 characters
 var badPass = "1234";
 var goodPass = "1234567890";
-utils.validPassword(badPass);
-utils.validPassword(goodPass);
-//assert.equal(utils.validPassword(badPass), false, ['invalid password']);
-//assert.equal(utils.validPassword(goodPass), true, ['valid password']);
+//utils.validPassword(badPass);
+//utils.validPassword(goodPass);
+assert.equal(utils.validPassword(badPass), true, ['invalid password']);
+assert.equal(utils.validPassword(goodPass), false, ['valid password']);
 
 //token test
 //invalid token returns false, needs to be 36 characters
