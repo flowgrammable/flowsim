fb1dd7533c272598174ac17831055e34ee4cb3b6
ojh302846 <oron.hazi@gmail.com>
2014-07-21 09:02:53 -0500

    Update controllerUtils.js

--- src/backend/rest/subscriber/controllerUtils.js
+++ src/backend/rest/subscriber/controllerUtils.js
@@ -3,31 +3,53 @@ var enforce = require('enforce');
 var checks = new enforce.Enforce();
 function validEmail(email){
 	//use node-enfore library
-	checks.add("testEmail", enforce.patterns.email("bad email"))
+	checks.add("testEmail", enforce.patterns.email({      
+	      'error': {
+	        'type': 'Registration',
+	        'description': 'Could not register user',
+	        'data': {
+	          'code': 1103,
+	          'message': 'Invalid Email'
+	        }
+	      }
+	    }));
 	checks.check({
 		testEmail : email
 	}, function (err) {
-		console.log(err);
+		if(err){
+			console.log(JSON.stringify(err.msg));
+		}
 	});
 }
 
 function validPassword(password){
-	checks.add("testPass", enforce.range.length(8, 16, "bad password"))
+	checks.add("testPass", enforce.range.length(8, 16, {
+	      'error': {
+	        'type': 'Registration',
+	        'description': 'Could not register user',
+	        'data': {
+	          'code': 1104,
+	          'message': 'Password is not 8-16 chars'
+	        }
+	      }
+	    }));
 	checks.check({
 		testPass : password
 	}, function (err) {
-		console.log(err);
+		if(err){
+			console.log(JSON.stringify(err.msg));
+		}
 	});
 }
 
-function validToken(token){
-	checks.add("testToken", enforce.range.length(36, 36, "bad token"))
-	checks.check({
-		testToken : token
-	}, function (err) {
-		console.log(err);
-	});
-}
+//function validToken(token){
+//	checks.add("testToken", enforce.range.length(36, 36, "bad token"))
+//	checks.check({
+//		testToken : token
+//	}, function (err) {
+//		console.log(err);
+//	});
+//}
 
 exports.validPassword = validPassword;
 exports.validEmail = validEmail;
