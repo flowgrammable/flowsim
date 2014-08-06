3caae5af0cd632ed8fadc95b30d5b46b44a0a1c7
Oron Hazi <oron.hazi@gmail.com>
2014-08-06 13:31:18 -0500

    Update utilsTest.js

--- src/backend/rest/profile/test/utilsTest.js
+++ src/backend/rest/profile/test/utilsTest.js
@@ -3,14 +3,6 @@ var assert = require('assert');
 describe('===> Utils Test: \n', function(){
 	var badData = {name: ''};
 	var noNameData = {notName: 'test'};
-	it('Packet: Test passing invalid name should return true', function(done){
-		assert.equal(utils.invalidPacket(badData), true);
-		done();
-	});
-	it('Packet: Test passing data with no "name" field should return true', function(done){
-		assert.equal(utils.invalidPacket(noNameData), true);
-		done();
-	});
 	it('Profile: Test passing invalid name should return true', function(done){
                 assert.equal(utils.invalidProfile(badData), true);
                 done();
