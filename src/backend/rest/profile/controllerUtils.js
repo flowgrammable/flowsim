fe2851571f1aa56b2a2743495c31c90790efc071
Oron Hazi <oron.hazi@gmail.com>
2014-08-06 13:27:41 -0500

    Update controllerUtils.js

--- src/backend/rest/profile/controllerUtils.js
+++ src/backend/rest/profile/controllerUtils.js
@@ -1,9 +1,4 @@
 
-function invalidPacket(data){
-	if(!data.name) return true;
-	else if(data.name.length === 0) return true;
-	else return false;
-}
 
 function invalidProfile(data) {
 	if(!data.name) return true;
@@ -11,5 +6,5 @@ function invalidProfile(data) {
         else return false;
 }
 
-exports.invalidPacket = invalidPacket;
+
 exports.invalidProfile = invalidProfile;
