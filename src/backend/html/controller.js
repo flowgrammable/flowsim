e87d6a5fffe8e308c0584806fabe54524e2a1fa4
Jasson Casey <jasson@flowgrammable.com>
2014-08-07 14:30:31 -0400

    Adding: notes

--- src/backend/html/controller.js
+++ src/backend/html/controller.js
@@ -1,6 +1,12 @@
 
 var _ = require('underscore');
 
+// Note
+// -------------------------------------------------------------------------
+// It would be nice if this could be refactored into a constructable functor.
+// Then this controller could be used as a typical connection service object.
+//
+
 exports.serve = function(app, connect, config) {
 
   _.each(config.content, function(value, key) { 
