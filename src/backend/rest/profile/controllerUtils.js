fe4f5b2a787179895aeac7e625e4a7aa85866172
Oron Hazi <oron.hazi@gmail.com>
2014-08-14 12:24:19 -0500

    update profile testing

--- src/backend/rest/profile/controllerUtils.js
+++ src/backend/rest/profile/controllerUtils.js
@@ -4,7 +4,11 @@ function invalidProfile(data) {
 	if(!data.name) return msg.missingName;
         else if(data.name.length === 0) return msg.missingName;
 	else if(!data.ofp_version) return msg.missingOfpVersion;
-	else if(data.ofp_version === 10 || data.ofp_version === 11 || data.ofp_version === 12 || data.ofp_version === 13 || data.ofp_version === 14) return msg.missingOfpVersion;;
+	else if(data.ofp_version === 10 || 
+		data.ofp_version === 11 || 
+		data.ofp_version === 12 || 
+		data.ofp_version === 13 || 
+		data.ofp_version === 14) return msg.missingOfpVersion;
         else return false;
 }
 
@@ -12,7 +16,11 @@ function invalidProfileUpdate(data) {
 	if(!data.name) return msg.missingName;
         else if(data.name.length === 0) return msg.missingName;
         else if(!data.ofp_version) return msg.missingOfpVersion;
-        else if(data.ofp_version === 10 || data.ofp_version === 11 || data.ofp_version === 12 || data.ofp_version === 13 || data.ofp_version === 14) return msg.missingOfpVersion;;
+        else if(data.ofp_version === 10 || 
+		data.ofp_version === 11 || 
+		data.ofp_version === 12 || 
+		data.ofp_version === 13 || 
+		data.ofp_version === 14) return msg.missingOfpVersion;
 	else if(!data.id) return msg.missingId;
 	else if(data.id.length === 0) return msg.missingId;
         else return false;
--- src/backend/rest/profile/test/restTest.js
+++ src/backend/rest/profile/test/restTest.js
@@ -110,69 +110,69 @@ describe('Testing update profile request: ', function() {
   	});
 	
 	before(function(done) {
-		this.timeout(5000);
-		request( { // register anoter subscriber
-			url: 'http://localhost:3000/api/subscriber/register',
-      			body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
-      			headers: { 'Content-Type': 'application/json' },
-      			method: 'POST'
-    		}, function (error, response, body) {
-      			console.log(body);
-      			assert(JSON.parse(body)['value'],'Unable to register user');
-      			fs.readFile(process.cwd()+'/temp','utf8',function (err,data) {
-        		if (err) console.log('Unable to read token in file for restTest');
-        		else {
-          			var array = data.toString().split("\n");
-          			token = JSON.parse(array[0]).ver_token;
-          			request( { // verify subscriber
-            				url: 'http://localhost:3000/api/subscriber/verify/',
-            				body: '{ \"token\": \"'+token+'\"}',
-            				headers: { 'Content-Type': 'application/json' },
-            				method: 'POST'
-          			}, function (error, response, body) {
-            				assert(JSON.parse(body)['value'],'Unable to verify user');
-            				request( { // login subscriber
-              					url: 'http://localhost:3000/api/subscriber/login',
-              					body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
-              					headers: { 'Content-Type': 'application/json' },
-              					method: 'POST'
-            				}, function (error, response, body) {
-              					assert(JSON.parse(body)['value'],'Unable to login user');
-              					sessKey = JSON.parse(body)['value'];
-              					request( { // make another profile
-      							url: 'http://localhost:3000/api/profile/create',
-      							body: '{ \"name\": \"test2profile\", \"ofp_version\": \"10\"}',
-      							headers: { 'Content-Type':'application/json', 'x-access-token': sessKey },
-      							method: 'POST'
-    	      					}, function (error, response, body) {
-      							assert(JSON.parse(body)['value'],'Unable to create profile');
-      							console.log('\tResponse received : ', body);
-      							request( { //logout
-      		  						url: 'http://localhost:3000/api/subscriber/logout',
-      		  						body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
-      		  						headers: {'Content-Type': 'application/json','x-access-token': sessKey},
-      		  						method: 'POST'
-    							}, function (error, response, body) {
-      		  						assert(JSON.parse(body)['value'],'Unable to logout user');
-      		  						console.log('\tResponse received : ', body);
-      	 	  						request( { // login subscriber 1
-             	    							url: 'http://localhost:3000/api/subscriber/login',
-               	    							body: '{ \"email\": \"flowgrammablemailer@gmail.com\", \"password\": \"my password\"}',
-              	    							headers: { 'Content-Type': 'application/json' },
-                    							method: 'POST'
-            	  						}, function (error, response, body) {
-                    							assert(JSON.parse(body)['value'],'Unable to login user');
-                    							sessKey = JSON.parse(body)['value'];
-                    							done();
-            	  						});
-    							});
-	      					});
-            				});
-          			});
-        		}
-      		});
-    	});
-  });
+	  this.timeout(5000);
+	  request( { // register anoter subscriber
+	    url: 'http://localhost:3000/api/subscriber/register',
+      	    body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+      	    headers: { 'Content-Type': 'application/json' },
+      	    method: 'POST'
+    	  }, function (error, response, body) {
+      	    console.log(body);
+      	    assert(JSON.parse(body)['value'],'Unable to register user');
+      	    fs.readFile(process.cwd()+'/temp','utf8',function (err,data) {
+            if (err) console.log('Unable to read token in file for restTest');
+            else {
+              var array = data.toString().split("\n");
+              token = JSON.parse(array[0]).ver_token;
+              request( { // verify subscriber
+            	url: 'http://localhost:3000/api/subscriber/verify/',
+            	body: '{ \"token\": \"'+token+'\"}',
+            	headers: { 'Content-Type': 'application/json' },
+            	method: 'POST'
+              }, function (error, response, body) {
+            	assert(JSON.parse(body)['value'],'Unable to verify user');
+                request( { // login subscriber
+              	url: 'http://localhost:3000/api/subscriber/login',
+              	body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+                headers: { 'Content-Type': 'application/json' },
+                method: 'POST'
+            	}, function (error, response, body) {
+              	  assert(JSON.parse(body)['value'],'Unable to login user');
+              	  sessKey = JSON.parse(body)['value'];
+              	  request( { // make another profile
+      		    url: 'http://localhost:3000/api/profile/create',
+      		    body: '{ \"name\": \"test2profile\", \"ofp_version\": \"10\"}',
+      		    headers: { 'Content-Type':'application/json', 'x-access-token': sessKey },
+      		    method: 'POST'
+    	      	  }, function (error, response, body) {
+      		    assert(JSON.parse(body)['value'],'Unable to create profile');
+      		    console.log('\tResponse received : ', body);
+      		    request( { //logout
+      		      url: 'http://localhost:3000/api/subscriber/logout',
+      		      body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+      		      headers: {'Content-Type': 'application/json','x-access-token': sessKey},
+      		      method: 'POST'
+    		    }, function (error, response, body) {
+      		      assert(JSON.parse(body)['value'],'Unable to logout user');
+      		      console.log('\tResponse received : ', body);
+      	 	      request( { // login subscriber 1
+             	        url: 'http://localhost:3000/api/subscriber/login',
+               	        body: '{ \"email\": \"flowgrammablemailer@gmail.com\", \"password\": \"my password\"}',
+              	        headers: { 'Content-Type': 'application/json' },
+                        method: 'POST'
+            	      }, function (error, response, body) {
+                    	assert(JSON.parse(body)['value'],'Unable to login user');
+                    	sessKey = JSON.parse(body)['value'];
+                    	done();
+            	      });
+    		    });
+	      	  });
+            	});
+              });
+             }
+      	   });
+    	 });
+       });
 	it('Test: PUT to /api/profile/update with subscriber_id in the body should return msg.notAuthorized()',
 	function(done) {
                 request( {
@@ -201,5 +201,46 @@ describe('Testing update profile request: ', function() {
                 });
         });
 
+});
+//-----------------------------------------------------------------------------
+//Test list profile
+describe('Testing list profile request: ', function(){
+  /*it('Test: Successful request of profiles GET /api/profile/list should return {"id":id, "name":name, "ofp_version": a_version}',
+  function(done) {
+  
+  });
+  it('Test: GET /api/profile/list with no profiles found should return msg.noProfilesFound()',
+  function(done) {
+
+  });
+  it('Test: List of profiles should only return {"id":id, "name":name, "ofp_version": a_version}',
+  function(done) {
+
+  });*/
+  before(function(done){
+    request( { //logout
+      url: 'http://localhost:3000/api/subscriber/logout',
+      body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+      headers: {'Content-Type': 'application/json','x-access-token': sessKey},
+      method: 'POST'
+    }, function (error, response, body) {
+      assert(JSON.parse(body)['value'],'Unable to logout user');
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
+  it('Test: GET /api/profile/list while not logged in should return msg.subscriberUnauthenticated()',
+  function(done) {
+    request( {
+      url: 'http://localhost:3000/api/profile/list',
+      body: '{ \"id\": \"999999999\", \"name\": \"test3profile\"}',
+      headers: { 'Content-Type':'application/json', 'x-access-token': sessKey },
+      method: 'PUT'
+    }, function (error, response, body) {
+      assert.equal(JSON.parse(body)['error']['type'],'profileNotFound');
+      console.log('\tResponse received : ', body);
+      done();
+    });
+  });
 });
 
