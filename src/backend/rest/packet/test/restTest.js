d035cac7334a2f843e04c8b370c42eb1716247f5
Oron Hazi <oron.hazi@gmail.com>
2014-08-19 12:15:16 -0500

    update packet restTest

--- src/backend/rest/packet/test/restTest.js
+++ src/backend/rest/packet/test/restTest.js
@@ -111,3 +111,92 @@ describe('Testing update profile request: ', function() {
 	*/
 });
 
+// -----------------------------------------------------------------------------
+// Testing list packet
+describe('Testing list packet request: ', function() {
+
+  it('Test: Successful request of profiles GET /api/packet/list should return {"id":id, "name":name} ',
+  function(done) {
+      request({
+        url : 'http://localhost:3000/api/packet/list',
+        headers: {'Content-Type': 'application/json','x-access-token': sessKey},
+        method: 'GET'
+      }, function (error, response, body) {
+        console.log(body);
+        assert(JSON.parse(body)['value'][0]);
+        assert(JSON.parse(body)['value'][0].id);
+        assert(JSON.parse(body)['value'][0].name);
+        done();
+    });
+  });
+
+  it('Test: GET /api/packet/list with no packet found should return {value: [ ] }',
+  function(done) {
+    request( { // register anoter subscriber
+      url: 'http://localhost:3000/api/subscriber/register',
+      body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log(body);
+      assert(JSON.parse(body)['value'],'Unable to register user');
+      fs.readFile(process.cwd()+'/temp','utf8',function (err,data) {
+      if (err) console.log('Unable to read token in file for restTest');
+      else {
+        var array = data.toString().split("\n");
+        token = JSON.parse(array[0]).ver_token;
+        request( { // verify subscriber
+          url: 'http://localhost:3000/api/subscriber/verify/',
+          body: '{ \"token\": \"'+token+'\"}',
+          headers: { 'Content-Type': 'application/json' },
+          method: 'POST'
+        }, function (error, response, body) {
+          assert(JSON.parse(body)['value'],'Unable to verify user');
+          request( { // login subscriber
+            url: 'http://localhost:3000/api/subscriber/login',
+            body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST'
+          }, function (error, response, body) {
+            assert(JSON.parse(body)['value'],'Unable to login user');
+            sessKey = JSON.parse(body)['value'];
+            request( {
+              url : 'http://localhost:3000/api/packet/list',
+              headers: {'Content-Type': 'application/json','x-access-token': sessKey},
+              method: 'GET'
+            }, function (error, response, body) {
+              assert(JSON.parse(body)['value'],'noPacketsFound');
+              console.log('\tResponse received : ', body);
+              done();
+            }); 
+          });
+        });
+      }
+      });
+    });
+  });
+
+  it('Test: GET /api/packet/list while not logged in should return msg.subscriberUnauthenticated()',
+  function(done) {
+    request( { //logout
+      url: 'http://localhost:3000/api/subscriber/logout',
+      body: '{ \"email\": \"test@flog.com\", \"password\": \"my_password\"}',
+      headers: {'Content-Type': 'application/json','x-access-token': sessKey},
+      method: 'POST'
+    }, function (error, response, body) {
+      assert(JSON.parse(body)['value'],'Unable to logout user');
+      console.log('\tResponse received : ', body);
+      request( {
+        url: 'http://localhost:3000/api/profile/list',
+        headers: { 'Content-Type':'application/json', 'x-access-token': sessKey },
+        method: 'PUT'
+      }, function (error, response, body) {
+        assert.equal(JSON.parse(body)['error']['type'],'subscriberUnauthenticated');
+        console.log('\tResponse received : ', body);
+        done();
+      });
+    });
+  });
+  
+});
+
