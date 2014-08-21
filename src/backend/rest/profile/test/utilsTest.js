26c5580ee5568f42677a6f63fa728abd3336661b
Thomas Wynn <T-Wynn@users.noreply.github.com>
2014-08-21 12:44:34 -0400

    fixed a bug with utils test

--- src/backend/rest/profile/test/utilsTest.js
+++ src/backend/rest/profile/test/utilsTest.js
@@ -1,14 +1,16 @@
 var utils = require('../controllerUtils.js');
+var msg = require('../msg.js');
 var assert = require('assert');
 describe('===> Utils Test: \n', function(){
   var missingNameData = {name: ''};
   var noNameData = {notName: 'test'};
   it('Profile: Test passing a missing name should return msg.missingName()', function(done){
-    assert.equal(utils.invalidProfile(missingNameData), true);
+    assert.equal(utils.invalidProfile(missingNameData).error.type, 'missingName');
     done();
   });
   it('Profile: Test passing data with no "name" field should return msg.missingName()', function(done){
-    assert.equal(utils.invalidProfile(noNameData), true);
+    assert.equal(utils.invalidProfile(missingNameData).error.type, 'missingName');
     done();
   });
 });
+
