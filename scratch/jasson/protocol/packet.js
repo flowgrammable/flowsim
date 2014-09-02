7a878adc694e68ab1933abfbc9d91f5faeac5812
Jasson Casey <jasson@flowgrammable.com>
2014-09-02 13:03:51 -0400

    latest

--- scratch/jasson/protocol/packet.js
+++ scratch/jasson/protocol/packet.js
@@ -1,7 +1,8 @@
 
 var formatter = require('./formatter');
 
-function Packet() {
+function Packet(name) {
+  this.name = name;
   this.protocols = [];
   this.bytes = 0;
 }
@@ -13,8 +14,10 @@ Packet.prototype.pushProtocol = function(p) {
 }
 
 Packet.prototype.popProtocol = function() {
-  if(this.protocols.length > 0)
+  if(this.protocols.length > 0) {
+    this.bytes -= this.protocols[this.protocols.length-1].bytes();
     this.protocols.splice(this.protocols.length-1, 1);
+  }
 }
 
 Packet.prototype.labels = function() {
@@ -22,5 +25,40 @@ Packet.prototype.labels = function() {
   for(var i=0; i<this.protocols.length; ++i) {
     result.push(this.protocols[i].labels());
   }
+  return {
+    name: this.name,
+    bytes: this.bytes,
+    protocols: result
+  }
+}
+
+function Payload(bytes) {
+  this._bytes = bytes;
+}
+exports.Payload = Payload;
+
+Payload.prototype.toFormatter = function(f) {
+  f.begin('Payload');
+  f.addPair('Bytes', this._bytes);
+  f.end();
+}
+
+Payload.prototype.toString = function() {
+  var f = new formatter.Formatter();
+  this.toFormatter(f);
+  var result = f.toString();
+  delete f;
   return result;
+}
+
+Payload.prototype.labels = function() {
+  return {
+    name: 'Payload',
+    bytes: this.bytes(),
+    fields: []
+  };
+}
+
+Payload.prototype.bytes = function() {
+  return this._bytes;
 }
--- scratch/jasson/protocol/test/testPacket.js
+++ scratch/jasson/protocol/test/testPacket.js
@@ -3,12 +3,14 @@ var packet = require('../packet');
 var ethernet = require('../ethernet');
 
 var eth = new ethernet.Header('01:11:22:33:44:55', 'ff:ff:ff:ff:ff:ff', 0x8100);
-var pkt = new packet.Packet(1522);
+var pkt = new packet.Packet('broadcast');
+var pld = new packet.Payload(1000);
 
 pkt.pushProtocol(eth);
+pkt.pushProtocol(pld);
 
 console.log(pkt.labels());
-var labels = pkt.labels()[0];
+var labels = pkt.labels().protocols[0];
 for(var i=0; i<labels.fields.length; ++i) {
   console.log(labels.fields[i].name + ': ' + labels.fields[i].value);
 }
