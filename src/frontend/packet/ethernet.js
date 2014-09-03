bdd11fa804fca03a80fa2ccfd1f249d96454e0a4
Jasson Casey <jasson@flowgrammable.com>
2014-09-03 13:50:14 -0400

    latest

--- src/frontend/packet/ethernet.js
+++ src/frontend/packet/ethernet.js
@@ -4,30 +4,71 @@ var Ethernet = (function(){
   var macp = /(([0-9a-fA-F]{2})(-|:)){5}([0-9a-fA-F]{2})/;
   var octp = /[0-9a-fA-F]{2}/g;
 
-  return {
+  var etherTypes = {
+    IPv4: 0x0800,
+    ARP: 0x0806,
+    RARP: 0x8035,
+    VLAN: 0x8100,
+    IPv6: 0x86DD,
+    MPLSu: 0x8847,
+    MPLSm: 0x8848,
+    LLDP: 0x88cc
+  };
 
-    Address : function(value) {
+  var AddressCons = function(value) {
       var result = [];
       if(value === undefined) {
         this.addr = [0, 0, 0, 0, 0, 0];
       } else if(typeof value == 'string' && macp.test(value)) {
         result = value.match(octp);
         this.addr = value.match(octp).slice(0,6);
-      } else if(value instanceof(Address)) {
+      } else if(value instanceof AddressCons) {
         this.addr = value.addr.slice(0);
       } else {
         throw 'Invalid MAC: ' + value;
       }
+  };
+
+  var HeaderCons = function(src, dst, type) {
+      if(src instanceof HeaderCons && dst === undefined && type === undefined) {
+        this.src = src.src.clone();
+        this.dst = src.dst.clone();
+        this.type = src.type;
+      } else {
+        this.src = new AddressCons(src);
+        this.dst = new AddressCons(dst);
+        this.type = type || 0;
+      }
+  };
+
+  return {
+    Address : AddressCons,
+    Header : HeaderCons,
+
+    etherTypeByName : function(name) {
+      if(name in etherTypes) {
+        return etherTypes[name];
+      } else {
+        return 'Unknown EtherType';
+      }
     },
 
-    Header : function(src, dst, type) {
-      this.src = new Address(src);
-      this.dst = new Address(dst);
-      this.type = type || 0;
+    etherTypeByValue : function(value) {
+      for(var prop in EtherTypes) {
+        if(EtherTypes.hasOwnProperty(prop)) {
+          if(value == EtherTypes[prop])
+            return prop;
+        }
+      }
+      return 'Unknown EtherType';
     }
 
   };
 })();
+
+Ethernet.Address.prototype.clone = function() {
+  return new Ethernet.Address(this);
+}
 
 Ethernet.Address.prototype.bytes = function() {
   return 6;
@@ -43,7 +84,13 @@ Ethernet.Address.prototype.isMulticast = function() {
   return this.addr[0] & 0x01;
 }
 
+Ethernet.Header.prototype.clone = function() {
+  return new Ethernet.Header(this);
+}
+
 Ethernet.Header.prototype.bytes = function() {
   return 14;
 }
 
+var e0 = new Ethernet.Header('00:11:22:33:44:55', 'ff:ff:ff:ff:ff:ff', 0);
+var e1 = e0.clone();
