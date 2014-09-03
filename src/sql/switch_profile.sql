468b1e5e0f45918560ae6458adae0eeb3dfbf339
Thomas Wynn <twynn3348@gmail.com>
2014-09-03 13:17:49 -0400

    added sequelize model file for switch_profile and removed the NOT NULL constraint from sql

--- src/sql/switch_profile.sql
+++ src/sql/switch_profile.sql
@@ -15,65 +15,56 @@ CREATE TABLE switch_profile
    * A subscriber can have many profiles
    * A profile can only belong to one subscriber
    */ 
-  subscriber_id INTEGER references subscriber(id) NOT NULL,
+  subscriber_id INTEGER references subscriber(id) /*NOT NULL*/,
 
   /*
    * Profile Name
    * example: 'brocade mlxe profile'
    */
-  name VARCHAR(60) NOT NULL,
+  name VARCHAR(60) /*NOT NULL*/,
 
   --------------------------- Datapath Capabilities ---------------------------  
   /*
    * Openflow protocol version
    */
-  ofp_version BYTEA  NOT NULL,
+  ofp_version BYTEA  /*NOT NULL*/,
 
   /*
    * datapath_id 
    * 
    */
-   datapath_id BYTEA NOT NULL,
+   datapath_id BYTEA /*NOT NULL*/,
 
   /*
 	 * n_buffers
    */
-   n_buffers BYTEA NOT NULL,
+   n_buffers BYTEA /*NOT NULL*/,
 
   /*
    * n_tables
    * number of flowtable in the datapath
    */
-   n_tables BYTEA NOT NULL, 
+   n_tables BYTEA /*NOT NULL*/, 
 
   /*
    * ip_reassembly
    * datapath can reassemble ip fragments
    */
-   ip_reassembly BOOLEAN NOT NULL, 
+   ip_reassembly BOOLEAN /*NOT NULL*/, 
 
   /*
    * stp
    * indicates whether a switch supports spanning-tree protocol
    */
-   stp BOOLEAN NOT NULL, 
+   stp BOOLEAN /*NOT NULL*/, 
 
   /*
    * port_blocked
    * loops will be blocked by an external (non openflow) protocol
    */   
-   port_blocked BOOLEAN NOT NULL,
+   port_blocked BOOLEAN /*NOT NULL*/,
 
   /* vendor_id/experimenter? */
-
-  /*
-   * version_in_use
-   * the ofp version being used
-   *
-   version_in_use INTEGER NOT NULL,
-   *
-   * ofp_version is same thing as this, commenting out - colton
-   */
   -----------------------------------------------------------------------------
   
   --------------------------- Datapath Description ----------------------------
@@ -81,31 +72,31 @@ CREATE TABLE switch_profile
    * mfr_description
    * manufacturer description
    */
-   mfr_description VARCHAR(60) NOT NULL,
+   mfr_description VARCHAR(60) /*NOT NULL*/,
 
   /*
    * hw_description
    * hardware description
    */
-   hw_description VARCHAR(60) NOT NULL,
+   hw_description VARCHAR(60) /*NOT NULL*/,
 
   /*
    * sw_description
    * software description
    */
-   sw_description VARCHAR(60) NOT NULL, 
+   sw_description VARCHAR(60) /*NOT NULL*/, 
 
   /*
    * serial_num
    * serial number
    */
-   serial_num VARCHAR(60) NOT NULL,
+   serial_num VARCHAR(60) /*NOT NULL*/,
 
   /*
    * dp_description
    * description of the datapath
    */
-   dp_description VARCHAR(60) NOT NULL,
+   dp_description VARCHAR(60) /*NOT NULL*/,
   -----------------------------------------------------------------------------
 
   -------------------------- Datapath Configuration ---------------------------
@@ -113,18 +104,18 @@ CREATE TABLE switch_profile
     * miss_send_len
     * max bytes of flows sent to controller
     */
-   miss_send_len INTEGER NOT NULL,
+   miss_send_len INTEGER /*NOT NULL*/,
    
    /*
     * frag_handling
     * 
     */
-   frag_handling IP_FRAG_HANDLING NOT NULL,
+   frag_handling IP_FRAG_HANDLING /*NOT NULL*/,
    
    /*
     * invalid_ttl_to_controller
     * 
     */
-   invalid_ttl_to_controller BOOLEAN NOT NULL,
+   invalid_ttl_to_controller BOOLEAN /*NOT NULL*/,
   ----------------------------------------------------------------------------
 );
