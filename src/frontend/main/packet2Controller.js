ae30e207463d1f899ed1bc6ad7ebeb5765122ea7
Jasson Casey <jasson@flowgrammable.com>
2014-09-03 11:22:58 -0400

    latest

--- src/frontend/main/packet2Controller.js
+++ src/frontend/main/packet2Controller.js
@@ -5,15 +5,50 @@ function Ethernet() {
   this.src = [0,0,0,0,0,0];
   this.dst = [0,0,0,0,0,0];
   this.ethertype = 0;
+
+  this.name = "Ethernet";
 }
 
 Ethernet.prototype.bytes = function() {
   return 14;
 }
 
+function ARP() {
+  this.name = "ARP";
+}
+
+function VLAN() {
+  this.name = "VLAN";
+}
+
+function MPLS() {
+  this.name = "MPLS";
+}
+
+function IPv4() {
+  this.name = "IPv4";
+}
+
+function IPv6() {
+  this.name = "IPv6";
+}
+
+function TCP() {
+  this.name = "TCP";
+}
+
+function UDP() {
+  this.name = "UDP";
+}
+
+function SCTP() {
+  this.name = "SCTP";
+}
+
 function Packet(name) {
   this.name = name;
   this.protocols = [new Ethernet()];
+  this.next = ['ARP', 'VLAN', 'MPLS', 'IPv4', 'IPv6'];
   this._bytes = this.protocols[0].bytes();
 }
 
@@ -32,6 +67,38 @@ Packet.prototype.popProtocol = function() {
     tmp = this.protocols.splice(this.protocols.length-1);
     this._bytes -= tmp.bytes();
     delete tmp;
+  }
+}
+
+Packet.prototype.addProtocol = function(name) {
+  switch(name) {
+    case "ARP":
+      this.pushProtocol(new ARP());
+      break;
+    case "VLAN":
+      this.pushProtocol(new VLAN());
+      break;
+    case "MPLS":
+      this.pushProtocol(new MPLS());
+      break;
+    case "IPv4":
+      this.pushProtocol(new IPv4());
+      break;
+    case "IPv6":
+      this.pushProtocol(new IPv6());
+      break;
+    case "TCP":
+      this.pushProtocol(new TCP());
+      break;
+    case "UDP":
+      this.pushProtocol(new UDP());
+      break;
+    case "SCTP":
+      this.pushProtocol(new SCTP());
+      break;
+    default:
+      console.log("Trying to add unknown protocol: " + name);
+      break;
   }
 }
 
@@ -86,9 +153,12 @@ flowsimApp.controller('packet2Controller',
     $scope.badPacketName = false;
     $scope.packetExists = false;
     $scope.focus = -1;
+    $scope.protocol = '';
+    $scope.packet = null;
 
     $scope.shiftFocus = function(pos) {
       $scope.focus = pos;
+      $scope.packet = $scope.packets.packets[pos];
     }
 
     $scope.addPacket = function() {
@@ -103,7 +173,7 @@ flowsimApp.controller('packet2Controller',
           $scope.badPacketName = false;
           $scope.packetExists = false;
           $scope.packetName = '';
-          $scope.focus = $scope.packets.size()-1;
+          $scope.shiftFocus($scope.packets.size()-1);
           break;
       }
     }
@@ -112,8 +182,12 @@ flowsimApp.controller('packet2Controller',
       if($scope.focus == pos)
         $scope.focus = -1;
       else if($scope.focus != -1 && pos <= $scope.focus)
-        $scope.focus -= 1;
+        $scope.shiftFocus(pos-1);
       $scope.packets.delPacketByPos(pos);
+    }
+
+    $scope.addProtocol = function() {
+      $scope.packet.addProtocol($scope.protocol);
     }
   });
 
