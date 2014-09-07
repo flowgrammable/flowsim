ecdf250d9a1bc0fc40317667af1b9e91535e51c0
Jasson Casey <jasson@flowgrammable.com>
2014-09-07 11:15:28 -0400

    latest

--- scratch/jasson/ui/Gruntfile.js
+++ scratch/jasson/ui/Gruntfile.js
@@ -1,5 +1,9 @@
 
 module.exports = function(grunt) {
-  // Do grunt-related things in here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  grunt.registerTask('default', []);
 };
 
