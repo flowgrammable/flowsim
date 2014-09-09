aac85b669d62d061955bc67dec29cac0701c393f
Jasson Casey <jasson@flowgrammable.com>
2014-09-09 17:49:07 -0400

    Adding gruntfile updates

--- scratch/jasson/frontend/gruntfile.js
+++ scratch/jasson/frontend/gruntfile.js
@@ -3,16 +3,31 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      options: {},
-      files: ['src/**/*.js']
+      files: ['gruntfile.js', 'src/**/*.js']
+    },
+    concat: {
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.loadNpmTasks('grunt-html2js');
 
-  grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'concat']);
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
 };
 
