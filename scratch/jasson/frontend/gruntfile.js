d239880c9d7169ce5f3e25a6316152433c6683d1
Jasson Casey <jasson@flowgrammable.com>
2014-09-10 10:30:07 -0400

    latest

--- scratch/jasson/frontend/gruntfile.js
+++ scratch/jasson/frontend/gruntfile.js
@@ -1,4 +1,6 @@
 
+var ejs = require('ejs');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -18,17 +20,49 @@ module.exports = function(grunt) {
           'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
+    },
+    indexgen: {
+      options: {
+        title: 'bleep',
+        body: 'blorp',
+        deps: grunt.file.readJSON('deps.json')
+      },
+      dist: {
+        files: {
+          'dist/index.html': ['src/index.ejs']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-html2js');
 
 
-  grunt.registerTask('default', ['jshint', 'concat']);
+  grunt.registerTask('default', ['jshint', 'copy', 'concat']);
   grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+
+  grunt.registerMultiTask('indexgen', 'Generate an index.html', function() {
+    var options = this.options();
+    options.styles = options.deps.devcss;
+    options.scripts = options.deps.devjs;
+    console.log(options);
+    this.files.forEach(function(file) {
+      var contents = file.src.filter(function(filepath) {
+        if (!grunt.file.exists(filepath)) {
+          console.log('Bad source template: ' + filepath);
+          return false;
+        }
+        return true;
+      }).map(function(filepath) {
+        return grunt.file.read(filepath);
+      });
+
+      grunt.file.write(file.dest, ejs.render(contents[0], options));
+    });
+  });
 };
 
