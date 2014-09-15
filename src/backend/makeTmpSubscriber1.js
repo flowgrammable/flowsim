ed2336e13ebe894fc3672dd276308a3e85937a00
Oron Hazi <oron.hazi@gmail.com>
2014-09-15 16:46:39 -0500

    Create makeTmpSubscriber1.js

---
+++ src/backend/makeTmpSubscriber1.js
@@ -0,0 +1,32 @@
+//Registers and verifies temporary user 1
+
+describe('Registering user 1', function() {
+  before(function(done) { 
+    this.timeout(5000);
+    request( { // register subscriber
+      url: 'http://localhost:3000/api/subscriber/register',
+      body: '{ \"email\": \"flowgrammabletest1@gmail.com\", \"password\": \"openflow1\"}',
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST'
+    }, function (error, response, body) {
+      console.log(body);
+      assert(JSON.parse(body)['value'],'Unable to register user');
+      fs.readFile(process.cwd()+'/temp','utf8',function (err,data) {
+        if (err) console.log('Unable to read token in file for restTest');
+        else {
+          var array = data.toString().split("\n"); 
+          token = JSON.parse(array[0]).ver_token;
+          request( { // verify subscriber
+            url: 'http://localhost:3000/api/subscriber/verify/',
+            body: '{ \"token\": \"'+token+'\"}',
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST'
+          }, function (error, response, body) { 
+            assert(JSON.parse(body)['value'],'Unable to verify user');
+            done();
+          });
+        }
+      });
+    });
+  });
+});
