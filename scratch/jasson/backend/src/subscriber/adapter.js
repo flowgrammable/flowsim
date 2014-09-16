bed57db41a2abed81b87591d818241d3b0bd84d4
Jasson Casey <jasson@flowgrammable.com>
2014-09-16 16:26:31 -0400

    Adding: refactored biz-logic out of adapter.js

--- scratch/jasson/backend/src/subscriber/adapter.js
+++ scratch/jasson/backend/src/subscriber/adapter.js
@@ -5,11 +5,11 @@ exports.module = function(db) {
 
 var database = db;
 
-function _createSubscriber(eml, pwd, ip, token, dispatch) {
+function _createSubscriber(eml, pwd, date, ip, token, dispatch) {
   database.table('subscriber').create({
     email: eml,
     password: pwd,
-    reg_date: new Date(),
+    reg_date: date,
     reg_ip: ip,
     verification_token: token,
     status: 'CREATED'
@@ -24,8 +24,82 @@ function _createSubscriber(eml, pwd, ip, token, dispatch) {
   });
 }
 
+function _fetchSubscriber(subInfo, dispatch) {
+  database.table('subscriber').find({
+    where: subInfo
+  }).success(function(result) {
+    if(result) {
+      dispatch(msg.success(result));
+    } else {
+      dispatch(msg.subscriberNotFound());
+    }
+  }).error(function(err) {
+    dispatch(msg.unknownError(err));
+  });
+}
+
+function _updateSubscriber(sub, newSubInfo, dispatch) {
+  database.table('subscriber').updateAttributes(newSubInfo)
+    .success(function(result) {
+      dispatch(msg.success(result));
+    }).error(function(err) {
+      dispatch(msg.unknownError(err));
+    });
+}
+
+function _createSession(sub, skey, tmo, distpatch) {
+  database.table('session').create({
+    key: skey,
+    subscriber_id: sub.id,
+    timeout: tmo
+  }).success(function(result) {
+    dispatch(msg.success(result.key));
+  }).error(function(err) {
+    dispatch(msg.unknownError(err));
+  });
+}
+
+function _fetchSession(skey, dispatch) {
+  database.table('session').find({
+    where: { key: skey } 
+  }).success(function(result) {
+    if(result) {
+      dispatch(msg.success(result));
+    } else {
+      dispatch(msg.sessionNotFound());
+    }
+  }).error(function(err) {
+    dispatch(msg.unknownError(err)); // probably db connection error
+  });
+}
+
+function _destroySession(session, dispatch) {
+  session.destroy()
+    .success(function(result) {
+      dispatch(msg.success());
+    }).error(function(err) {
+    dispatch(msg.unknownError(err));
+  });
+}
+
+function _destroySessionLessThan(time, dispatch) {
+  database.table('session').destroy({
+    timeout: { lt: time } 
+  }).success(function(result) { 
+    dispatch(msg.success(result));
+  }).error  (function(err) { 
+    dispatch(msg.error(err));
+  });
+}
+
 return {
-  createSubscriber: _createSubscriber
+  createSubscriber: _createSubscriber,
+  fetchSubscriber: _fetchSubscriber,
+  updateSubscriber: _updateSubscriber,
+  createSession: _createSession,
+  fetchSession: _fetchSession,
+  destroySession: _destroySession,
+  destroySessionLessThan: _destroySessionLessthan
 };
 
 }
