77d54952f4819b32a2a7eb3154c6a168bf56c44c
Jasson Casey <jasson@flowgrammable.com>
2014-09-18 12:12:39 -0400

    Making mailer inline with new style

--- scratch/jasson/backend/src/mailer/index.js
+++ scratch/jasson/backend/src/mailer/index.js
@@ -1,18 +1,23 @@
 
-var nm = require('nodemailer');
+(function(){
 
-module.exports = function(cfg) {
-  var name = 'mailer';
+var nm  = require('nodemailer');
+var fmt = require('../utils/formatter');
+
+var name = 'mailer';
+
+function Mailer(config) {
 
   // Grab a configuration if present ...
   // ... otherwise throw an error
-  var config = cfg.get(name);
-  if(!config) {
+  this.config = config.get(name);
+  if(!this.config) {
     // FIXME: add a better error mechanism
     throw 'Mailer missing config';
   }
 
-  var transporter = nm.createTransport({
+  // initialize the nodemailer transporter
+  this.transporter = nm.createTransport({
     service: config.service,
     auth: {
       user: config.user,
@@ -20,40 +25,39 @@ module.exports = function(cfg) {
     }
   });
 
-  function _close() {
-    transporter.close();
-  }
-  
-  function _mail(dst, sub, body) {
-    transporter.sendMail({
-      from: config.user,                    // set the smtp from
-      to: dst,                              // set the smtp to
-      subject: sub,                         // set the smtp subject
-      html: body                            // set the smtp html-body
-    }, function(err, info) {
-      if(err) {
-        console.log('Mailer error');
-        console.log(err);
-      } else {
-        console.log('Mailer success');
-        console.log(info);
-      }
-    });
-  }
+}
+exports.Mailer = Mailer;
 
-  function _toString() {
-    return "";
-  }
-
-  function _toFormatter(f) {
-    return f;
-  }
-
-  return {
-    mail: _mail,
-    close: _close,
-    toString: _toString,
-    toFormatter: _toFormatter
-  };
+Mailer.prototype.close = function() {
+  this.transporter.close();
 };
+
+Mailer.prototype.mail = function(dst, sub, body, delegate) {
+  transporter.sendMail({
+    from: this.config.user,   // set the smtp from
+    to: dst,                  // set the smtp to
+    subject: sub,             // set the smtp subject
+    html: body                // set the smtp html-body
+  }, function(err, info) {
+    if(delegate && err) {
+      delegate(err);
+    } else if(delegate && info) {
+      delegate(info);
+    } else if(err) {
+      //FIXME: this needs to be something better than console
+      console.log(err);
+    }
+  });
+}; 
+
+Mailer.prototype.toFormatter = function(f) {
+  f.begin('Mailer');
+  f.addPair('Server', this.config.service);
+  f.addPair('User', this.config.user);
+  f.end();
+};
+
+Mailer.prototype.toString = fmt.toString;
+
+})();
 
