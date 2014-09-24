de45cf5f2fd11a210877a92edebc9248e3e8071d
colton <coltonchojnacki@gmail.com>
2014-09-23 22:05:10 -0500

    changed date format to iso

--- scratch/jasson/backend/src/subscriber/test/test.js
+++ scratch/jasson/backend/src/subscriber/test/test.js
@@ -12,7 +12,11 @@ var db = new pg.Database({database:{
 
 var store = new st.Storage(db);
 
-store.createSubscriber('j@j', '123', new Date(), '1.1.1.1', '090909', 
+// Date must be passed in as ISO-8601 string
+var d = new Date();
+var dISO = d.toISOString();
+
+store.createSubscriber('j@j', '123', dISO, '1.1.1.1', '090909', 
   function(err, result) {
     if(err) {
       console.log(err);
