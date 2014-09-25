fd2617bf3c6f1c8d24206f212f3e6fedbed9013d
colton <coltonchojnacki@gmail.com>
2014-09-24 23:57:22 -0500

    space test2

--- scratch/jasson/backend/src/subscriber/test/errorCodes.js
+++ scratch/jasson/backend/src/subscriber/test/errorCodes.js
@@ -4,10 +4,10 @@ var pg = require('../../database/pg');
 var st = require('./../storage');
 
 var db = new pg.Database({database:{
-    user: 'flogdev',
-    pwd: 'flogdev',
-    host: 'localhost',
-    database: 'flowsim'
+  user: 'flogdev',
+  pwd: 'flogdev',
+  host: 'localhost',
+  database: 'flowsim'
 }});
 
 var store = new st.Storage(db);
@@ -16,30 +16,30 @@ var d = new Date();
 var dISO = d.toISOString();
 
 function genString(n){
-    var y = '';
-    for(i = 0; i < n; i++)
-        y+= 'a';	
-    return y;
+  var y = '';
+  for(i = 0; i < n; i++)
+      y+= 'a';	
+  return y;
 };
 
 // email longer than 128 chars
 var badLength = genString(200);
 store.createSubscriber(badLength, '123', dISO, '1.1.1.1', uuid.v4(), 
-    function(err, result){
-        if(err) {
-            console.log('createSub error');
-            console.log('Email length greater than 128');
-            console.log(err);
-        } else {
-            console.log(result)
-        }
+  function(err, result){
+    if(err) {
+      console.log('createSub error');
+      console.log('Email length greater than 128');
+      console.log(err);
+    } else {
+      console.log(result)
+    }
 });
 
 // password longer than 60 chars
 var badPass = genString(61);
 store.createSubscriber('hi', badPass, dISO, '1.1.1.1', uuid.v4(),
-	function(err, result){
-		if(err) {
+  function(err, result){
+  	if(err) {
 			console.log('createSub error');
       console.log('Password length greater than 60');
 			console.log(err);
@@ -50,25 +50,25 @@ store.createSubscriber('hi', badPass, dISO, '1.1.1.1', uuid.v4(),
 
 // badpass and bad email length
 store.createSubscriber(uuid.v4() , 'hi' , dISO, '11.1.1', uuid.v4(),
-	function(err, result){
-		if(err){
-			console.log('createSub error');
+  function(err, result){
+    if(err){
+      console.log('createSub error');
       console.log('Incorrect IP address syntax: 11.1.1');
-			console.log(err);
-		} else {
-			console.log(result);
-		}
+      console.log(err);
+    } else {
+      console.log(result);
+    }
 });
 
 //bad Date format (javascript date)
 store.createSubscriber(uuid.v4(), 'hi', new Date(), '1.1.1.1', uuid.v4(),
-	function(err, result){
-		if(err){
-			console.log('createSub error');
-			console.log('Invalid date format: javascript Date()');
-			console.log(err);
-		} else {
-			console.log(result);
-		}
+  function(err, result){
+    if(err){
+      console.log('createSub error');
+      console.log('Invalid date format: javascript Date()');
+      console.log(err);
+    } else {
+      console.log(result);
+    }
 });
 db.close();
