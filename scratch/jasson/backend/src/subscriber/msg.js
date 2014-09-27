60ec6105de9f2563c47c9eed367d0c08fc48a2dd
Jasson Casey <jasson@flowgrammable.com>
2014-09-27 09:43:19 -0400

    Changing: error handling

--- scratch/jasson/backend/src/subscriber/msg.js
+++ scratch/jasson/backend/src/subscriber/msg.js
@@ -5,27 +5,6 @@
 
 (/** @lends module:subscriber */function(){
 
-exports.scrub = function(logger) {
-  return function(msg) {
-    if(logger) {
-      logger.log(msg);
-    }
-    switch(msg.type) {
-      case "noDatabaseConnection":
-        return msg = {
-          system: "Server Error",
-          type: ""
-        };
-        break;
-      default:
-        break;
-    }
-    if(logger) {
-      logger.log(msg);
-    }
-  };
-}
-
 exports.methodNotSupported = function() {
 	return {
 		system: "subscriber/controller",
@@ -34,166 +13,166 @@ exports.methodNotSupported = function() {
 };
 
 exports.noDatabaseConnection = function() {
-  return msg.error({
+  return {
     system: "subscriber/adapter",
     type: "noDatabaseConnection"
-  });
+  };
 };
 
 exports.unknownError = function() {
-  return msg.error({
+  return {
     system: "subscriber/adapter",
     type: "unknownError"
-  });
+  };
 };
 
 exports.subscriberNotFound = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "subscriberNotFound"
-  });
+  };
 };
 
 exports.emailInUse = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "emailInUse"
-  });
+  };
 };
 
 exports.missingEmail = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "missingEmail"
-  });
+  };
 };
 
 exports.badEmail = function(email) {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "badEmail",
     email: email
-  });
+  };
 };
 
 exports.incorrectPwd = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "incorrectPwd"
-  });
+  };
 };
 
 exports.missingPwd = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "missingPwd"
-  });
+  };
 };
 
 exports.badPwd = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "badPwd"
-  });
+  };
 };
 
 exports.missingResetToken = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "missingResetToken"
-  });
+  };
 };
 
 exports.badVerificationToken = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "badVerificationToken"
-  });
+  };
 };
 
 exports.missingVerificationToken = function() {
-	return msg.error({
+  return {
 		system: "subscriber/controller",
 		type: "missingVerificationToken"
-	});
+	};
 };
 
 exports.badResetToken = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "badResetToken"
-  });
+  };
 };
 
 exports.missingAccessToken = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "missingAccessToken"
-  });
+  };
 };
 
 exports.badAccessToken = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "badAccessToken"
-  });
+  };
 };
 
 exports.accessTokenExpired = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "accessTokenExpired"
-  });
+  };
 };
 
 exports.subscriberNotActive = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "subscriberNotActive"
-  });
+  };
 };
 
 exports.subscriberNotReset = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "subscriberNotReset"
-  });
+  };
 };
 
 exports.subscriberClosed = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "subscriberClosed"
-  });
+  };
 };
 
 // replace with subscriberNotActive?
 exports.unverifiedSubscriber = function() {
-  return msg.error({
+  return {
     system: "subscriber/model",
     type: "unverifiedSubscriber"
-  });
+  };
 };
 
 exports.subscriberAlreadyVerified = function() {
-  return msg.error({
+  return {
     system: "subscriber/controller",
     type: "subscriberAlreadyVerified"
-  });
+  };
 };
 
 exports.badConfig = function() {
-  return msg.error({
+  return {
     system: "subscriber/adapter",
     type: "badEmailConfiguration"
-  });
+  };
 };
 
 exports.sessionNotFound = function() {
-  return msg.error({
+  return {
     system: "subscriber/adapter",
     type: "sessionNotFound"
-  });
+  };
 };
 
 })();
--- scratch/jasson/backend/src/utils/msg.js
+++ scratch/jasson/backend/src/utils/msg.js
@@ -9,20 +9,49 @@
  * @function success
  * @param {object} data - blah
  */
-exports.success = function(data) {
+function _success(data) {
   return {
     value: data || {}
   };
-};
+}
+exports.success = _success;
 
 /**
  * @function error
  * @param {object} data - blah
  */
-exports.error = function(data) {
+function _error(data) {
   return {
     error: data || {}
   };
+}
+exports.error = _error;
+
+exports.wrap = function(err, succ) {
+  if(err) {
+    return _error(err);
+  } else {
+    return _success(succ);
+  }
+};
+
+exports.filter = function(filter, pre, post) {
+  var _filter, _pre, _post;
+
+  _filter = filter || function(id) { return id; };
+  _pre  = pre      || null;
+  _post = post     || null;
+
+  return function(msg) {
+    if(_pre) {
+      _pre(msg);
+    }
+    msg = _filter(msg);
+    if(_post) {
+      _post(msg);
+    }
+    return msg;
+  }
 };
 
 })();
