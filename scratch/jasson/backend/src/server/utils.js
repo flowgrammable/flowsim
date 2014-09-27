046c2e5f49fa8514a4c9e00ec4f1824a7118212d
Jasson Casey <jasson@flowgrammable.com>
2014-09-27 10:06:13 -0400

    Changing: error

--- scratch/jasson/backend/src/server/utils.js
+++ scratch/jasson/backend/src/server/utils.js
@@ -1,13 +1,37 @@
 
 (function(){
 
+function _error(data) {
+  return {
+    error: data || {};
+  };
+}
+
+function _success(data) {
+  return {
+    value: data || {};
+  };
+}
+
+function _wrap(err, result) {
+  if(err) {
+    return _error(err);
+  } else {
+    return _success(result);
+  }
+}
+
 function Delegate(res) {
   var response = res;
-  return function(result) {
+  return function(err, result) {
     response.writeHead(200, {
       'Content-Type': 'application/json'
     });
-    response.end(JSON.stringify(result));
+    if(err) {
+      response.end(JSON.stringify(_wrap(err)));
+    } else {
+      response.end(JSON.stringify(_wrap(result)));
+    }
   };
 }
 exports.Delegate = Delegate;
