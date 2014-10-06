8bf4c14555123988d88b2e1a546de710ccbe1898
Jasson Casey <jasson@flowgrammable.com>
2014-10-06 12:53:47 -0400

    latest

--- frontend/src/subscriber/subscriber.js
+++ frontend/src/subscriber/subscriber.js
@@ -30,6 +30,25 @@ function checkPassword(scope, name) {
 }
 
 angular.module('fgSubscriber', ['ngResource'])
+  .factory('SubscriberRest', function($resource) {
+    var _authToken = '';
+    return {
+      get: function() { return _authToken; },
+      set: function(t) { _authToken = t; },
+      ops: $resource('/api/subscriber/:op', {
+          op: '@op'
+        }, {
+          'register': { method: 'POST', params: { op: 'register' } },
+          'verify':   { method: 'POST', params: { op: 'verify' } },
+          'forgot':   { method: 'POST', params: { op: 'forgot' } },
+          'login':    { method: 'POST', params: { op: 'login' } },
+          'logout':   { method: 'POST', params: { op: 'logout' },
+                        headers: { 'x-access-token': _x_access_token } },
+          'update':   { method: 'POST', params: { op: 'update' },
+                        headers: { 'x-access-token': _x_access_token } }
+        })
+    };
+  })
   .controller('fgSubAuth', function($scope, SubscriberRest) {
 
     $scope.logout = function() {
