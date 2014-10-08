3f6c7f926057dfbce9fe49d0d87bac5d33444481
Jasson Casey <jasson@flowgrammable.com>
2014-10-07 23:05:59 -0400

    latest

--- flowsim-ui/app/scripts/directives/fglist.js
+++ flowsim-ui/app/scripts/directives/fglist.js
@@ -8,11 +8,72 @@
  */
 angular.module('flowsimUiApp')
   .directive('fgList', function () {
+
     return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the fgList directive');
+      restrict: 'E',                      // HTML Element
+      transclude: true,                   // Copy element body in
+      templateUrl: 'fglist.html',         // Location of template
+      scope: {
+        onAdd: '&',                       // callback for adding item
+        onDel: '&',                       // callback for deleting item
+        onSet: '&'                        // callback for changing item focus
+      },
+
+      controller: function($scope) {
+        $scope.itemName = '';             // input name to create item
+        $scope.focus = -1;                // item with current focus
+        $scope.errorOccurred = false;     // input name error state
+        $scope.errorMessage = '';         // input name error message
+        
+        $scope.items = [];                // display list of items
+        $scope.init = false;              // dislay list initialization state
+        
+        $scope.$on('initList', function(event, data) {
+          $scope.items = data;
+          $scope.init = true;
+          if(data.length > 0) {
+            $scope.shiftFocus(0);
+          }
+        });
+
+        $scope.clearState = function() {
+          $scope.itemName = '';
+          $scope.errorOccurred = false;
+          $scope.errorMessage = '';
+        };
+        
+        $scope.shiftFocus = function(pos) {
+          if(pos >= -1 && pos < $scope.items.length) {
+            $scope.focus = pos;                   // Update the local focus
+            $scope.onSet()($scope.items[pos]);    // Update the parent with name
+          }
+        };
+        
+        $scope.addItem = function() {
+          var result = $scope.onAdd()($scope.itemName);
+          if(result == 'success') {
+            $scope.items.push($scope.itemName);
+            $scope.shiftFocus($scope.items.length-1);
+            $scope.clearState();
+          } else {
+            $scope.errorOccurred = true;
+            $scope.errorMessage = result;
+          }
+        };
+        
+        $scope.delItem = function(pos) {
+          var item;
+          if(pos >= -1 && pos < $scope.items.length) {
+            item = $scope.items.splice(pos, 1); 
+            if(pos < $scope.focus) {
+              $scope.shiftFocus($scope.focus-1);
+            } else if(pos <= $scope.focus) {
+              $scope.shiftFocus($scope.focus);
+            }
+            $scope.onDel()(item);
+          }
+        };
       }
     };
   });
+
