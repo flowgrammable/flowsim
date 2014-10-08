123b0407d0b49ab191c6723aa23d37a9f0f51a4c
Jasson Casey <jasson@flowgrammable.com>
2014-10-08 16:01:34 -0400

    latest

---
+++ flowsim-ui/app/scripts/services/constraints.js
@@ -0,0 +1,8 @@
+
+(function() {
+
+function validateInt(val) {
+}
+
+})();
+
--- flowsim-ui/app/scripts/services/vlan.js
+++ flowsim-ui/app/scripts/services/vlan.js
@@ -1,8 +1,43 @@
 'use strict';
 
+function validateUInt(lb, ub) {
+  var pat = /^[0-9]+$/;
+  return function(val) {
+    if(!pat.test(val)) {
+      return false;
+    }
+    var _val = parseInt(val);
+    if(typeof _val !== 'number' || !isFinite(_val) || _val%1 !== 0 || _val < lb || 
+              _val > ub) {
+      return false;
+    }
+    return true;
+  };
+}
+
 function _VLAN() {
   this.name = 'VLAN';
-  this.attrs = [];
+  this.attrs = [{
+    name: 'PCP',
+    value: 0,
+    test: validateUInt(0, 7),
+    tip: 'VLAN Priority Code Point'
+  }, {
+    name: 'DEI',
+    value: 0,
+    test: validateUInt(0,1),
+    tip: 'VLAN Drop Eligibility Indicator'
+  }, {
+    name: 'VLAN ID',
+    value: 0,
+    test: validateUInt(0, 4095),
+    tip: 'VLAN Tag Identifier'
+  }, {
+    name: 'TypeLen',
+    value: 0,
+    test: validateUInt(0, 65536),
+    tip: 'Ethernet type or length of payload'
+  }];
 }
 
 _VLAN.prototype.bytes = function() {
