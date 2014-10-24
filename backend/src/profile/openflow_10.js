f679d3c7a37e4ba673401ebb534d5b9ddb60cfa9
Colton Chojnacki <coltonchojnacki@gmail.com>
2014-10-23 21:04:47 -0500

    update

--- backend/src/profile/openflow_10.js
+++ backend/src/profile/openflow_10.js
@@ -2,38 +2,67 @@
 
 var assert = require('assert');
 var msg = require('./msg');
-var _   = require('underscore');
+var async  = require('async');
+var _ = require('underscore');
 var profUtils = require('./utils');
 
 
 /**
- * Datapath fields that are used in every version of
- * openflow
+ * Port fields used in openflow 1.0
  **/
 var portFields = {
   id : 'number',
   hw_addr : 'string',
   name : 'string'
-}
+};
 
-function validatePort(port, ports){
-  var errMsg;
-  _.every(portFields, function(v, i){
-    errMsg = utils.validateField(i, v, port);
-    return !errMsg;
+/**
+ * Ports fields used in openflow 1.0
+ **/
+var portsFields = {
+  vp_normal : 'bool',
+  vp_flood  : 'bool'
+};
+
+function validatePort(bodyPort, profile, cb){
+  var port = {};
+  async.eachSeries(Object.keys(portFields), function(field, callback){
+    profUtils.validateField(field, portFields[field],
+        bodyPort, port, callback);
+  }, function(err){
+    if(err){
+      cb(err);
+    } else {
+      profile.ports.push(port);
+      cb();
+    }
   });
-  return errMsg;
 }
 
-function validate(profile, reqBody, cb){
+function validatePorts(body, profile, cb){
+  async.eachSeries(body.ports, function(bodyPort, callback){
+    validatePort(bodyPort, profile, callback);
+  }, cb);
+}
+
+function validate(reqBody, profile, cb){
   var errMsg;
   // validate 1.0 Ports
   profile.ports = [];
-  _.every(reqBody.ports, function(v, i){
-    errMsg = validatePort(v,profile.ports);
-    return !errMsg;
-  });
-
+  async.waterfall([
+    function(callback){
+      validatePorts(reqBody, profile, callback);
+    },
+    function(callback){
+      console.log('hit next func');
+    }
+    ], function(err, result){
+        if(err){
+          cb(err)
+        } else {
+          console.log('result', result);
+        }
+    });
 }
 
 exports.validate = validate;
--- backend/src/profile/utils.js
+++ backend/src/profile/utils.js
@@ -3,16 +3,19 @@
 var assert = require('assert');
 var msg = require('./msg');
 var _   = require('underscore');
+var async = require('async');
 var of10 = require('./openflow_10');
 
 
-function validateField(fieldName, type, reqBody, cb){
+function validateField(fieldName, type, reqBody, prof, cb){
   if(!_.has(reqBody, fieldName)){
-    return msg.missingField(fieldName);
+    cb(msg.missingField(fieldName));
   } else if(typeof reqBody[fieldName] !== type){
-    return msg.invalidFieldType(fieldName, type)
+    cb(msg.invalidFieldType(fieldName, type));
+  } else {
+    prof[fieldName] = reqBody[fieldName];
+    cb();
   }
-  return false;
 }
 exports.validateField = validateField;
 
@@ -26,7 +29,7 @@ var dpFields = {
   ofp_version : 'number',
   datapath_id : 'number',
   n_buffers   : 'number',
-  n_tables   : 'number',
+/**  n_tables   : 'number',
   n_ports     : 'number',
   mfr_desciption : 'string',
   hw_description : 'string',
@@ -35,30 +38,30 @@ var dpFields = {
   dp_description : 'string',
   frag_handling  : 'string',
   ip_reassembly : 'boolean',
-  port_blocked  : 'boolean',
+  port_blocked  : 'boolean' */
+  ports : 'object'
   };
 
 function validateProfile(reqBody, cb){
   var profile = {};
   var errMsg;
   // first validate fields applicable to all versions
-  _.every(dpFields, function(v, i){
-    errMsg = validateField(i, v, reqBody);
-    return !errMsg;
-  });
-  // then validate fields by version
-  if(errMsg){
-    cb(errMsg);
-  } else {
-    switch(profile.ofp_version){
-      case 0:
-        of10.validate(reqBody, profile, cb);
-        break;
-      default:
-        cb(msg.badValue('ofp_version must be 0-4'));
-        break;
+  async.eachSeries(Object.keys(dpFields), function(field, callback){
+    validateField(field, dpFields[field], reqBody, profile, callback);
+  }, function(err){
+    if(err){
+      cb(err);
+    } else {
+      switch(profile.ofp_version){
+        case 0:
+          of10.validate(reqBody, profile, cb);
+          break;
+        default:
+          cb(msg.badValue('ofp_version must be 0-4'));
+          break;
+      }
     }
-  }
+  });
 }
 
 exports.validateProfile = validateProfile;
