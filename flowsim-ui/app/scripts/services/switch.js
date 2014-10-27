45297ee6128205fe899cb6aaa1795ab8f57ac09a
Jasson Casey <jasson@flowgrammable.com>
2014-10-27 15:59:04 -0400

    latest

--- flowsim-ui/app/scripts/services/switch.js
+++ flowsim-ui/app/scripts/services/switch.js
@@ -9,31 +9,70 @@
  */
 angular.module('flowsimUiApp')
   .factory('Switch', function _switch() {
-    var init = false;
-    var names = [];
     var switches = {};
-    return {
-      create: function(name) {
-        switches[name] = {
-          name: name
-        };
-      },
-      destroy: function(name) {
-        if(name in switches) {
-          delete switches[name];
-        }
-      },
-      get: function(name) {
-        return name in switches ? switches[name] : null;
-      },
-      getNames: function(callback) {
-        if(!init) {
-          names = [];
-          init = true;
-        }
-        callback(null, names);
-      },
-      save: function(callback) {
+
+    function get(name, callback) {
+      if(name in switches) {
+        callback(null, switches[name]);
+      } else {
+        Subscriber.httpGet('/api/switch/'+name, {}, function(err, result) {
+          if(err) {
+            callback(err);
+          } else {
+            switches[name] = result;
+            // attach
+            callback(null, result);
+          }
+        });
       }
+    }
+
+    function getNames(callback) {
+      if(Object.keys(switches).length) {
+        callback(null, Object.keys(switches));
+      } else {
+        Subscriber.httpGet('/api/switch', {}, function(err, result) {
+          callback(err, result);
+        });
+      }
+    }
+
+    function create(name) {
+      switches[name] = {};
+      switches[name].dirty = true;
+      return switches[name];
+    }
+
+    function destroy(name) {
+      delete switches[name];
+      Subscriber.httpDelete('/api/switch/'+name, {}, function(err, result) {
+        if(err) {
+          console.log(err.details);
+        } else {
+        }
+      });
+    }
+
+    function save() {
+      _.each(switchess, function(value, key) {
+        if(value.dirty) {
+          Subscriber.httpUpdate('/api/switch/'+key, value,
+                                function(err, result) {
+            if(err) {
+              console.log(err.details);
+            } else {
+              value.dirty = false;
+            }
+            });
+        }
+      });
+    }
+
+    return {
+      get: get,
+      getNames: getNames,
+      create: create,
+      destroy: destroy,
+      save: save
     };
   });
