a30fa2e9e4de5ca6882adb561cd15f789d29a8d4
Jasson Casey <jasson@flowgrammable.com>
2014-10-29 15:21:09 -0400

    latest

--- flowsim-ui/app/scripts/services/packet.js
+++ flowsim-ui/app/scripts/services/packet.js
@@ -8,6 +8,32 @@
  * Service in the flowsimUiApp.
  */
 
+angular.module('flowsimUiApp')
+  .factory('Packet', function(ETHERNET, VLAN, ARP, MPLS, IPV4, IPV6, ICMPV6,
+                              TCP, UDP, SCTP, PAYLOAD) {
+
+function Protocol(name) {
+  this.name = name;
+  this.bytes = bytes;
+  this.fields = [];
+}
+
+Protocol.prototype.push = function(field) {
+  this.fields.push(field);
+  this.bytes += field.bytes;
+};
+
+Protocol.prototype.pop = function() {
+  if(this.field.length === 0)
+    return;
+
+  this.bytes -= this.fields[this.fields.length-1].bytes;
+  this.fields.splice(this.fields.length-1);
+};
+
+function ProtocolUI(protocol) {
+}
+
 function Packet(name) {
   this.name = name;
   this.bytes = 0;
@@ -37,20 +63,18 @@ PacketUI.prototype.toBase = function() {
   return result;
 }
 
-angular.module('flowsimUiApp')
-  .factory('Packet', function(Protocols) {
 
-    function create(name) {
-      return new Packet(name);
-    }
+function create(name) {
+  return new Packet(name);
+}
 
-    function createUI(pkt) {
-      return new PacketUI(pkt);
-    }
+function createUI(pkt) {
+  return new PacketUI(pkt);
+}
 
-    return {
-      create: create,
-      createUI: createUI
-    };
+return {
+  create: create,
+  createUI: createUI
+};
 
-  });
+});
