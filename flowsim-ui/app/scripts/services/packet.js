0f4016fca227aeb1b6f41c99fb69c34d93221524
Jasson Casey <jasson@flowgrammable.com>
2014-10-29 15:15:29 -0400

    latest

--- flowsim-ui/app/scripts/services/packet.js
+++ flowsim-ui/app/scripts/services/packet.js
@@ -10,7 +10,22 @@
 
 function Packet(name) {
   this.name = name;
+  this.bytes = 0;
+  this.protocols = [];
 }
+
+Packet.prototype.push = function(protocol) {
+  this.protocols.push(protocol);
+  this.bytes += protocol.bytes;
+};
+
+Packet.prototype.pop = function() {
+  if(this.protocols.length === 0)
+    return;
+
+  this.bytes -= this.protocols[this.protocols.length-1].bytes;
+  this.prototocols.splice(this.protocols.length-1);
+};
 
 function PacketUI(pkt) {
   this.name = pkt.name;
