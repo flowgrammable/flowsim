80d75d643dcbe532b55dd98d2126b945c7c5895e
Jasson Casey <jasson@flowgrammable.com>
2014-10-30 23:42:17 -0400

    latest

---
+++ flowsim-ui/app/scripts/controllers/profile/table.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowsimUiApp.controller:ProfileTableCtrl
+ * @description
+ * # ProfileTableCtrl
+ * Controller of the flowsimUiApp
+ */
+angular.module('flowsimUiApp')
+  .controller('ProfileTableCtrl', function ($scope) {
+    $scope.match = function() {
+      console.log('match');
+    };
+    $scope.instruction = function() {
+      console.log('instruction');
+    };
+    $scope.instructionMiss = function() {
+      console.log('instruction miss');
+    };
+  });
