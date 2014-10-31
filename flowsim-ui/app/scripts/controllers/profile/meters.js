f64dd0a390a175ebdbef8a748d2eef33f34ace59
Jasson Casey <jasson@flowgrammable.com>
2014-10-31 12:01:47 -0400

    adding controllers

---
+++ flowsim-ui/app/scripts/controllers/profile/meters.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowsimUiApp.controller:ProfileMetersCtrl
+ * @description
+ * # ProfileMetersCtrl
+ * Controller of the flowsimUiApp
+ */
+angular.module('flowsimUiApp')
+  .controller('ProfileMetersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
