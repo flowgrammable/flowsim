dfb3487816a3dc02ffeb4529e26686f908e678d9
Jasson Casey <jasson@flowgrammable.com>
2014-10-30 23:45:35 -0400

    latest

---
+++ flowsim-ui/app/scripts/controllers/dialog/profile/instruction.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowsimUiApp.controller:DialogProfileInstructionCtrl
+ * @description
+ * # DialogProfileInstructionCtrl
+ * Controller of the flowsimUiApp
+ */
+angular.module('flowsimUiApp')
+  .controller('DialogProfileInstructionCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ flowsim-ui/test/spec/controllers/dialog/profile/instruction.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: DialogProfileInstructionCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('flowsimUiApp'));
+
+  var DialogProfileInstructionCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    DialogProfileInstructionCtrl = $controller('DialogProfileInstructionCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
