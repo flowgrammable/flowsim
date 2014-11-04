c1ba429b3d0bd70905d3ea80648119bfbc87dcd6
Jasson Casey <jasson@flowgrammable.com>
2014-11-04 11:41:51 -0500

    Initial skeleton for simulation

--- flowsim-ui/app/scripts/controllers/simulation.js
+++ flowsim-ui/app/scripts/controllers/simulation.js
@@ -9,9 +9,67 @@
  */
 angular.module('flowsimUiApp')
   .controller('SimulationCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.active = false;
+    $scope.stages = [{
+      name: 'Packet Arrival',
+      label: 'arrival',
+      active: false
+    }, {
+      name: 'Field Extraction',
+      label: 'extraction',
+      active: false
+    }, {
+      name: 'Table Selection',
+      label: 'choice',
+      active: false
+    }, {
+      name: 'Flow Selection',
+      label: 'selection',
+      active: false
+    }, {
+      name: 'Instruction Execution',
+      label: 'execution',
+      active: false
+    }];
+    
+    $scope.play = function() {
+      console.log('play');
+      if($scope.active) {
+        return;
+      }
+      $scope.active = true;
+      $scope.stages[0].active = true;
+    };
+
+    $scope.stop = function() {
+      console.log('stop');
+      if(!$scope.active) {
+        return;
+      }
+      $scope.active = false;
+    };
+
+    $scope.pause = function() {
+      console.log('pause');
+      if(!$scope.active) {
+        return;
+      }
+    };
+
+    $scope.step = function() {
+      console.log('step');
+      var idx;
+      if(!$scope.active) {
+        return;
+      }
+      for(idx=0; idx<$scope.stages.length; ++idx) {
+        if($scope.stages[idx].active) {
+          $scope.stages[idx].active = false;
+          $scope.stages[(idx+1)%$scope.stages.length].active = true;
+          return;
+        }
+      }
+    };
+
   });
