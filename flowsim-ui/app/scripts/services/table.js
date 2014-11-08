195ededc8d7068e5b9cb0b4abd56744c226659e2
Colton Chojnacki <coltonchojnacki@gmail.com>
2014-11-08 10:18:11 -0600

    update

--- flowsim-ui/app/scripts/services/table.js
+++ flowsim-ui/app/scripts/services/table.js
@@ -8,7 +8,47 @@
  * Service in the flowsimUiApp.
  */
 angular.module('flowsimUiApp')
-  .factory('Table', function table() {
+  .factory('Table', function table(Match, Instruction, fgConstraints) {
+
+var defaultTableStats = true;
+var defaultFlowStats  = true;
+var defaultMissController = false;
+var defaultMissContinue = false;
+var defaultMissDrop = false;
+var defaultMaxEntries = 1024;
+
+Table.Profile = function(tbl){
+  if(tbl instanceof Table.Profile || (typeof tbl === 'object' && tbl !== null)){
+    _.extend(this, tbl);
+    this.match = new Match.Profile(tbl.match);
+    this.instruction = new Instruction.Profile(tbl.instruction);
+    this.miss = new Instruction.Profile(tbl.miss);
+  } else {
+    this.table_id = tbl;
+    this.name = 'table' + this.table_id;
+    this.max_entries = defaultMaxEntries;
+    this.table_stats = defaultTableStats;
+    this.flow_stats = defaultFlowStats;
+    this.match = new Match.Profile();
+    this.instruction = new Instruction.Profile();
+    this.miss = new Instruction.Profile();
+  }
+
+}
+
+Table.Profile.TIPS = {
+  Match: Match.Profile.TIPS,
+  Instruction: Instruction.Profile.TIPS,
+  Miss: Instruction.Profile.TIPS
+}
+
+Table.Profile.TESTS = {
+  name: function(n) { return /^[a-zA-Z_][a-zA-Z0-9_]*$/.test(n); },
+  max_entries: fgConstraints.isUInt(0,0xffffffff),
+  Match: Match.Profile.TESTS,
+  Instruction: Instruction.Profile.TESTS,
+  Miss: Instruction.Profile.TESTS
+};
 
 function Priority(priority) {
   if(priority && typeof priority === 'number') {
@@ -45,14 +85,22 @@ Priority.prototype.select = function(key) {
   return null;
 };
 
-function Table(table) {
-  if(table) {
+function Table(table, profile) {
+  if(table instanceof Table || (typeof table === 'object' && table !== null)) {
     _.extend(this, table);
-    this.priorities = _.map(table.priorities, function(priority) { 
+    this.capabilities.match = new Match.Profile(table.capabilities.match);
+    this.capabilities.instruction = new Instruction.Profile(table.capabilities.instruction);
+    this.capabilities.miss = new Instruction.Profile(table.capabilities.miss);
+    this.priorities = _.map(table.priorities, function(priority) {
       return priority.clone();
     });
     this.miss = table.miss.clone();
   } else {
+    this.capabilities = {
+      match: profile.match,
+      instruction: profile.instruction,
+      miss: profile.miss
+    }
     this.priorities = [];
     this.miss = null;
   }
@@ -93,7 +141,8 @@ Table.prototype.select = function(key) {
 };
 
 return {
-  Table: Table
+  Table: Table,
+  Profile: Table.Profile
 };
 
 });
--- flowsim-ui/app/scripts/services/tables.js
+++ flowsim-ui/app/scripts/services/tables.js
@@ -8,33 +8,18 @@
  * Service in the flowsimUiApp.
  */
 angular.module('flowsimUiApp')
-  .factory('Tables', function(fgConstraints, ETHERNET) {
+  .factory('Tables', function(fgConstraints, ETHERNET, Instruction, Match, Table) {
 
 /* Default Construction Constants */
 var defaultTables     = 8;
-var defaultMaxEntries = 1024;
-var defaultTableStats = true;
-var defaultFlowStats  = true;
-var defaultMissController = false;
-var defaultMissContinue = false;
-var defaultMissDrop = false;
 
-
-function createMatch(protocol, field, key, wildcard, maskable, mask) {
-  return {
-    protocol: protocol,
-    field: field,
-    key: key,
-    enabled: true,
-    wildcardable: wildcard,
-    maskable: maskable,
-    mask: mask
-  };
-}
 
 function Tables(tbls, profile) {
   if(tbls instanceof Tables || (typeof tbls === 'object' && tbls !== null)){
     _.extend(this, tbls);
+    this.tables = _.map(tbls.tables, function(table){
+      return new Tables.Table(table);
+    });
   } else {
     this.tables = _.map(profile.tables, function(table){
       return new Tables.Table(null, table);
@@ -43,446 +28,33 @@ function Tables(tbls, profile) {
 }
 
 
-
-Tables.Table = function(table, profileTable){
-  if(table instanceof Tables.Table ||
-    (typeof table ==='object' && table !== null)){
-      _.extend(this, table);
-      this.capabilities = _.clone(table.capabilities);
-    } else {
-      this.capabilities = {
-        match : profileTable.match,
-        instruction : profileTable.instruction,
-        miss : profileTable.miss
-      };
-      this.flows = [];
-      this.miss_controller = defaultMissController;
-      this.miss_continue = defaultMissContinue;
-      this.miss_drop = defaultMissDrop;
-    }
-}
-
-function mkActionField(name, value, key) {
-  return {
-    name: name,
-    value: value,
-    key: key
-  };
-}
-
-
-Profile.Table = function(tbl, id){
-  if(tbl instanceof Profile.Table || (typeof tbl === 'object' && tbl !== null)){
-    _.extend(this, tbl);
-    this.match = new Profile.Table.Match(tbl.match);
-    this.instruction = new Profile.Table.Instruction(tbl.instruction);
-    this.miss = new Profile.Table.Miss(tbl.miss);
-  } else {
-    this.table_id = tbl;
-    this.name = 'table' + this.table_id;
-    this.max_entries = defaultMaxEntries;
-    this.table_stats = defaultTableStats;
-    this.flow_stats = defaultFlowStats;
-    this.match = new Profile.Table.Match();
-    this.instruction = new Profile.Table.Instruction();
-    this.miss = new Profile.Table.Miss;
-  }
-}
-
-Profile.Table.Match = function(mat, match){
-  if(mat instanceof Profile.Table.Match || (typeof mat === 'object' && mat !==null)){
-    _.extend(this, mat);
-    this.fields = _.map(mat.fields, function(f) { return _.clone(f); });
-  } else {
-    this.fields = [
-      createMatch('Ingress', 'Port', 'in_port', true, false, 0),
-      createMatch('Ethernet', 'Src', 'eth_src', true, true, '0xffffffffffff'),
-      createMatch('Ethernet', 'Dst', 'eth_dst', true, true, '0xffffffffffff'),
-      createMatch('Ethernet', 'Type/Len', 'eth_typelen', true, false, '0xffff'),
-      createMatch('ARP', 'Opcode', 'arp_opcode', true, false, 0),
-      createMatch('ARP', 'SHA', 'arp_sha', true, false, '0xffffffffffff'),
-      createMatch('ARP', 'SPA', 'arp_spa', true, true, '0xffffffff'),
-      createMatch('ARP', 'THA', 'arp_tha', true, true, '0xffffffffffff'),
-      createMatch('ARP', 'TPA', 'arp_tpa', true, true, '0xffffffff'),
-      createMatch('VLAN', 'PCP', 'vlan_pcp', true, true, '0x7'),
-      createMatch('VLAN', 'DEI', 'vlan_dei', true, true, '0x3'),
-      createMatch('VLAN', 'VID', 'vlan_vid', true, true, '0x0fff'),
-      createMatch('VLAN', 'Type/Len', 'vlan_typelen', true, true, '0xffff'),
-      createMatch('MPLS', 'Label', 'mpls_label', true, true, '0x0fffff'),
-      createMatch('MPLS', 'Traffic Control', 'mpls_tc', true, true, '0x7'),
-      createMatch('MPLS', 'BOS', 'mpls_bos', true, true, '0x0fffff'),
-      createMatch('IPv4', 'DSCP', 'ipv4_dscp', true, true, '0x3f'),
-      createMatch('IPv4', 'ECN', 'ipv4_ecn', true, true, '0x03'),
-      createMatch('IPv4', 'Proto', 'ipv4_proto', true, true, '0xff'),
-      createMatch('IPv4', 'Src', 'ipv4_src', true, true, '0xffffffff'),
-      createMatch('IPv4', 'Dst', 'ipv4_dst', true, true, '0xffffffff'),
-      createMatch('IPv6', 'Src', 'ipv6_src', true, true,
-        '0xffffffffffffffffffffffffffffffff'),
-      createMatch('IPv6', 'Dst', 'ipv6_dst', true, true,
-        '0xffffffffffffffffffffffffffffffff'),
-      createMatch('IPv6', 'Flow Label', 'ipv6_flabel', true, true,
-        '0xfffff'),
-      createMatch('ICMPv6', 'Type', 'icmpv6_type', true, true, '0xff'),
-      createMatch('ICMPv6', 'Code', 'icmpv6_code', true, true, '0xff'),
-      createMatch('ICMPv4', 'Type', 'icmpv4_type', true, true, '0xff'),
-      createMatch('ICMPv4', 'Code', 'icmpv4_code', true, true, '0xff'),
-      createMatch('TCP', 'Src', 'tcp_src', true, true, '0xffff'),
-      createMatch('TCP', 'Dst', 'tcp_dst', true, true, '0xffff'),
-      createMatch('UDP', 'Src', 'udp_src', true, true, '0xffff'),
-      createMatch('UDP', 'Dst', 'udp_dst', true, true, '0xffff'),
-      createMatch('SCTP', 'Src', 'sctp_src', true, true, '0xffff'),
-      createMatch('SCTP', 'Dst', 'sctp_dst', true, true, '0xffff')
-    ];
-  }
-}
-
-Profile.Table.Match.TIPS = {
-  in_port: 'Match on ingress port',
-  eth_src: 'Match on Ethernet source address',
-  eth_dst: 'Match on Ethernet destination address',
-  eth_typelen: 'Match on Ethernet type/length field',
-  arp_opcode: 'Match on ARP message type',
-  arp_sha: 'Match on Source hardware address',
-  arp_spa: 'Match on Source protocol address',
-  arp_tha: 'Match on Target hardware address',
-  arp_tpa: 'Match on Target protocol address',
-  vlan_pcp: 'Match on VLAN priority code',
-  vlan_dei: 'Match on VLAN',
-  vlan_vid: 'Match on VLAN ID',
-  vlan_typelen: 'Match on VLAN Type/Len',
-  mpls_label: 'Match on MPLS label',
-  mpls_tc: 'Match on MPLS tc',
-  mpls_bos: 'Match on MPLS bos',
-  ipv4_dscp: 'Match on IPv4 Differentiated Services Code Type',
-  ipv4_ecn: 'Match on Explicit Congestion Notification',
-  ipv4_proto: 'Match on Protocol',
-  ipv4_src: 'Match on IPv4 source',
-  ipv4_dst: 'Match on IPv4 destination',
-  ipv6_dst: 'Match on IPv6 destination',
-  ipv6_src: 'Match on IPv6 source',
-  tcp_src: 'Match on TCP source',
-  tcp_dst: 'Match on TCP destination',
-  udp_src: 'Match on UDP source',
-  udp_dst: 'Match on UDP destination',
-  sctp_src: 'Match on SCTP source',
-  sctp_dst: 'Match on SCTP destination'
-};
-
-Profile.Table.Match.TESTS = {
-  in_port: fgConstraints.isUInt(0, 0xffffffff),
-  eth_src: fgConstraints.isUInt(0, 0xffffffffffff),
-  eth_dst: fgConstraints.isUInt(0, 0xffffffffffff),
-  eth_typelen: fgConstraints.isUInt(0, 0xffff),
-  arp_opcode: fgConstraints.isUInt(0, 0x1),
-  arp_sha: fgConstraints.isUInt(0, 0xffffffffffff),
-  arp_spa: fgConstraints.isUInt(0, 0xffffffff),
-  arp_tha: fgConstraints.isUInt(0, 0xffffffffffff),
-  arp_tpa: fgConstraints.isUInt(0, 0xffffffff),
-  vlan_pcp: fgConstraints.isUInt(0, 0x7),
-  vlan_dei: fgConstraints.isUInt(0, 0x3),
-  vlan_vid: fgConstraints.isUInt(0, 0x0fff),
-  vlan_typelen: fgConstraints.isUInt(0, 0xffff),
-  mpls_label: fgConstraints.isUInt(0, 0x0fffff),
-  mpls_tc: fgConstraints.isUInt(0, 0x7),
-  mpls_bos: fgConstraints.isUInt(0, 0x0fffff),
-  ipv4_dscp: fgConstraints.isUInt(0, 0x3f),
-  ipv4_ecn: fgConstraints.isUInt(0, 0x03),
-  ipv4_proto: fgConstraints.isUInt(0, 255),
-  ipv4_src: fgConstraints.isUInt(0, 0xffffffff),
-  ipv4_dst: fgConstraints.isUInt(0, 0xffffffff),
-  icmpv4_type: fgConstraints.isUInt(0, 0xff),
-  icmpv4_code: fgConstraints.isUInt(0, 0xff),
-  icmpv6_type: fgConstraints.isUInt(0, 0xff),
-  icmpv6_code: fgConstraints.isUInt(0, 0xff),
-  ipv6_src: fgConstraints.isUInt(0,
-    0xffffffffffffffffffffffffffffffff),
-  ipv6_dst: fgConstraints.isUInt(0,
-    0xffffffffffffffffffffffffffffffff),
-  tcp_src: fgConstraints.isUInt(0, 0xffff),
-  tcp_dst: fgConstraints.isUInt(0, 0xffff),
-  udp_src: fgConstraints.isUInt(0, 0xffff),
-  udp_dst: fgConstraints.isUInt(0, 0xffff),
-  sctp_src: fgConstraints.isUInt(0, 0xffff),
-  sctp_dst: fgConstraints.isUInt(0, 0xffff)
-};
-
-Profile.Table.Instruction = function(ins, instruction){
-  if(ins instanceof Profile.Table.Instruction ||
-    (typeof ins === 'object' && ins !== null)){
-    _.extend(this, ins);
-    this.caps = _.clone(ins.caps);
-    this.apply = _.map(ins.apply, function(i) {
-      return {
-        protocol: i.protocol,
-        fields: _.map(i.fields, function(j) {
-          return _.clone(j);
-        })
-      };
-    });
-    this.write = _.map(ins.write, function(i){
-      return {
-        protocol: i.protocol,
-        fields: _.map(i.fields, function(j) {
-          return _.clone(j);
-        })
-      };
-    });
-    this.goto_ = _.map(ins.goto_, function(i) {
-      return _.clone(i);
+Tables.Profile = function(prof, tables){
+  if(prof instanceof Tables.Profile || (typeof prof ==='object' && prof !== null)){
+    _.extend(this, prof);
+    this.tables = _.map(prof.tables, function(table) {
+      return new Table.Profile(table);
     });
   } else {
-    this.caps = {
-      apply    : true,
-      clear    : true,
-      write    : true,
-      metadata : true,
-      meter    : true,
-      goto_    : true
-    };
-    this.apply = [{
-      protocol: 'Internal',
-      fields: [
-        mkActionField('Output', true, 'forward'),
-        mkActionField('Drop', true, 'drop'),
-        mkActionField('Set Group', true, 'set_group'),
-        mkActionField('Set Queue', true, 'set_queue')
-      ]
-    }, {
-      protocol: 'Ethernet',
-        fields: [
-          mkActionField('Src write', true, 'set_eth_src'),
-          mkActionField('Dst write', true, 'set_eth_dst'),
-          mkActionField('Typelen write', true, 'set_eth_type')
-        ]
-    }, {
-    protocol: 'ARP',
-      fields: [
-        mkActionField('Opcode write', true, 'set_arp_op'),
-        mkActionField('SHA write', true, 'set_arp_sha'),
-        mkActionField('SPA write', true, 'set_arp_spa'),
-        mkActionField('THA write', true, 'set_arp_tha'),
-        mkActionField('TPA write', true, 'set_arp_tpa')
-      ]
-    }, {
-      protocol: 'MPLS',
-      fields: [
-        mkActionField('Label write', true, 'set_mpls_label'),
-        mkActionField('TC write', true, 'set_mpls_tc'),
-        mkActionField('BOS write', true, 'set_mpls_bos'),
-        mkActionField('Set TTL', true, 'set_mpls_ttl'),
-        mkActionField('Dec TTL', true, 'dec_mpls_ttl'),
-        mkActionField('Push Tag', true, 'push_mpls'),
-        mkActionField('Pop Tag', true, 'pop_mpls')
-      ]
-    }, {
-      protocol: 'VLAN',
-      fields: [
-        mkActionField('PCP write', true, 'set_vlan_pcp'),
-        mkActionField('DEI write', true, 'set_vlan_dei'),
-        mkActionField('VID write', true, 'set_vlan_vid'),
-        mkActionField('Push tag', true, 'push_vlan'),
-        mkActionField('Pop tag', true, 'pop_vlan')
-      ]
-    }, {
-      protocol: 'IPv4',
-      fields: [
-        mkActionField('DSCP write', true, 'set_ip_dscp'),
-        mkActionField('ECN write', true, 'set_ip_ecn'),
-        mkActionField('Proto write', true, 'set_ip_proto'),
-        mkActionField('Src write', true, 'set_ipv4_src'),
-        mkActionField('Dst write', true, 'set_ipv4_dst'),
-        mkActionField('Set TTL', true, 'set_nw_ttl'),
-        mkActionField('Dec TTL', true, 'dec_nw_ttl')
-      ]
-    }, {
-      protocol: 'IPv6',
-      fields: [
-        mkActionField('Src write', true, 'set_ipv6_src'),
-        mkActionField('Dst write', true, 'set_ipv6_dst'),
-        mkActionField('FLabel write', true, 'set_ipv6_flabel')
-      ]
-    }, {
-      protocol: 'ICMPv4',
-      fields: [
-        mkActionField('Type write', true, 'set_icmpv4_type'),
-        mkActionField('Code write', true, 'set_icmpv4_code')
-      ]
-    }, {
-      protocol: 'ICMPv6',
-      fields: [
-        mkActionField('Type write', true, 'set_icmpv6_type'),
-        mkActionField('Code write', true, 'set_icmpv6_code')
-      ]
-    }, {
-      protocol: 'TCP',
-      fields: [
-        mkActionField('Src write', true, 'set_tcp_src'),
-        mkActionField('Dst write', true, 'set_tcp_dst')
-      ]
-    }, {
-      protocol: 'UDP',
-      fields: [
-        mkActionField('Src write', true, 'set_udp_src'),
-        mkActionField('Dst write', true, 'set_udp_dst')
-      ]
-    }, {
-      protocol: 'SCTP',
-      fields: [
-        mkActionField('Src write', true, 'set_sctp_src'),
-        mkActionField('Dst write', true, 'set_sctp_dst')
-      ]
-    }];
-    this.write = [{
-      protocol: 'Internal',
-      fields: [
-        mkActionField('Output', true, 'forward'),
-        mkActionField('Drop', true, 'drop'),
-        mkActionField('Set Group', true, 'set_group'),
-        mkActionField('Set Queue', true, 'set_queue')
-      ]
-    }, {
-      protocol: 'Ethernet',
-        fields: [
-          mkActionField('Src write', true, 'set_eth_src'),
-          mkActionField('Dst write', true, 'set_eth_dst'),
-          mkActionField('Typelen write', true, 'set_eth_type')
-        ]
-    }, {
-    protocol: 'ARP',
-      fields: [
-        mkActionField('Opcode write', true, 'set_arp_op'),
-        mkActionField('SHA write', true, 'set_arp_sha'),
-        mkActionField('SPA write', true, 'set_arp_spa'),
-        mkActionField('THA write', true, 'set_arp_tha'),
-        mkActionField('TPA write', true, 'set_arp_tpa')
-      ]
-    }, {
-      protocol: 'MPLS',
-      fields: [
-        mkActionField('Label write', true, 'set_mpls_label'),
-        mkActionField('TC write', true, 'set_mpls_tc'),
-        mkActionField('BOS write', true, 'set_mpls_bos'),
-        mkActionField('Set TTL', true, 'set_mpls_ttl'),
-        mkActionField('Dec TTL', true, 'dec_mpls_ttl'),
-        mkActionField('Push Tag', true, 'push_mpls'),
-        mkActionField('Pop Tag', true, 'pop_mpls')
-      ]
-    }, {
-      protocol: 'VLAN',
-      fields: [
-        mkActionField('PCP write', true, 'set_vlan_pcp'),
-        mkActionField('DEI write', true, 'set_vlan_dei'),
-        mkActionField('VID write', true, 'set_vlan_vid'),
-        mkActionField('Push tag', true, 'push_vlan'),
-        mkActionField('Pop tag', true, 'pop_vlan')
-      ]
-    }, {
-      protocol: 'IPv4',
-      fields: [
-        mkActionField('DSCP write', true, 'set_ip_dscp'),
-        mkActionField('ECN write', true, 'set_ip_ecn'),
-        mkActionField('Proto write', true, 'set_ip_proto'),
-        mkActionField('Src write', true, 'set_ipv4_src'),
-        mkActionField('Dst write', true, 'set_ipv4_dst'),
-        mkActionField('Set TTL', true, 'set_nw_ttl'),
-        mkActionField('Dec TTL', true, 'dec_nw_ttl')
-      ]
-    }, {
-      protocol: 'IPv6',
-      fields: [
-        mkActionField('Src write', true, 'set_ipv6_src'),
-        mkActionField('Dst write', true, 'set_ipv6_dst'),
-        mkActionField('FLabel write', true, 'set_ipv6_flabel')
-      ]
-    }, {
-      protocol: 'ICMPv4',
-      fields: [
-        mkActionField('Type write', true, 'set_icmpv4_type'),
-        mkActionField('Code write', true, 'set_icmpv4_code')
-      ]
-    }, {
-      protocol: 'ICMPv6',
-      fields: [
-        mkActionField('Type write', true, 'set_icmpv6_type'),
-        mkActionField('Code write', true, 'set_icmpv6_code')
-      ]
-    }, {
-      protocol: 'TCP',
-      fields: [
-        mkActionField('Src write', true, 'set_tcp_src'),
-        mkActionField('Dst write', true, 'set_tcp_dst')
-      ]
-    }, {
-      protocol: 'UDP',
-      fields: [
-        mkActionField('Src write', true, 'set_udp_src'),
-        mkActionField('Dst write', true, 'set_udp_dst')
-      ]
-    }, {
-      protocol: 'SCTP',
-      fields: [
-        mkActionField('Src write', true, 'set_sctp_src'),
-        mkActionField('Dst write', true, 'set_sctp_dst')
-      ]
-    }];
-    this.metadata = '0xffffffffffffffff';
-    this.goto_ = [];
+    this.n_tables = defaultTables;
+    this.tables = _.map(_.range(this.n_tables), function(id) {
+      return new Table.Profile(id);
+    });
   }
 }
-Profile.Table.Miss = Profile.Table.Instruction;
-//Tables.Table.Miss = Tables.Table.Instruction;
 
-Profile.Table.Instruction.TIPS = {
-  apply: 'Applies actions immediately',
-  clear: 'Clears the action set',
-  write: 'Appends actions to the action set',
-  metadata: 'Writes metadata register',
-  meter: 'Sends packet to designated meter',
-  goto_: 'Jumps to another flow table'
-};
-
-Profile.Table.Instruction.TESTS = {
-  metadata: fgConstraints.isUInt(0, 0xffffffffffffffff),
-  goto_: function(input) {
-    return /^([0-9]+)(\.\.([0-9]+))?$/.test(input);
-  }
-};
-
-Profile.Table.TIPS = {
+Tables.Profile.TIPS = {
   table_id: 'Unique table identifier',
   name: 'Descriptive name for flow table type',
   max_entries: 'Maximum flows supported',
   table_stats: 'Ability of table to record lookup statistics',
   flow_stats: 'Ability of flow to record match statistics',
   flow_caps: 'Match, Instruction, and Actions to support',
-  Match: Profile.Table.Match.TIPS,
-  Instruction: Profile.Table.Instruction.TIPS,
-  Miss: Profile.Table.Instruction.TIPS
+  Match: Match.Profile.TIPS,
+  Instruction: Instruction.Profile.TIPS,
+  Miss: Instruction.Profile.TIPS
 };
 
-Profile.Table.TESTS = {
-  name: function(n) { return /^[a-zA-Z_][a-zA-Z0-9_]*$/.test(n); },
-  max_entries: fgConstraints.isUInt(0,0xffffffff),
-  Match: Profile.Table.Match.TESTS,
-  Instruction: Profile.Table.Instruction.TESTS,
-  Miss: Profile.Table.Instruction.TESTS
-};
-
-function Profile(prof, tables){
-  if(prof instanceof Profile || (typeof prof ==='object' && prof !== null)){
-    _.extend(this, prof);
-    this.tables = _.map(prof.tables, function(table) {
-      return new Profile.Table(table);
-    });
-  } else {
-    this.n_tables = defaultTables;
-    this.tables = _.map(_.range(this.n_tables), function(id) {
-      return new Profile.Table(id);
-    });
-  }
-}
-
-Profile.prototype.rebuild = function() {
+Tables.Profile.prototype.rebuild = function() {
   var i;
   if(this.n_tables === this.tables.length) {
     return;
@@ -490,7 +62,7 @@ Profile.prototype.rebuild = function() {
     this.tables.splice(this.n_tables, this.tables.length-this.n_tables);
   } else {
     for(i=this.tables.length; i<this.n_tables; ++i) {
-      this.tables.push(new Profile.Table(i));
+      this.tables.push(new Table.Profile(i));
     }
   }
 };
@@ -756,12 +328,12 @@ Capabilities.prototype.openflow_1_4 = function() {
 
 var TIPS = {
   n_tables: 'Number of flow tables available',
-  Table: Profile.Table.TIPS
+  Table: Table.Profile.TIPS
 };
 
 var TESTS = {
   n_tables: fgConstraints.isUInt(0,0xff),
-  Table: Profile.Table.TESTS
+  Table: Table.Profile.TESTS
 };
 
 Tables.prototype.clone = function() {
@@ -772,7 +344,7 @@ var TablesUI = Tables;
 TablesUI.prototype.toBase = Tables.prototype.clone;
 
 return {
-  Capabilities: Profile,
+  Capabilities: Tables.Profile,
   Configuration: Tables,
   TIPS: TIPS,
   TESTS: TESTS
