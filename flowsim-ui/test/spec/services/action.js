510eaa52154b18016b57227a149692f90186f53b
Jasson Casey <jasson@flowgrammable.com>
2014-11-09 22:38:48 -0500

    latest

--- flowsim-ui/test/spec/services/action.js
+++ flowsim-ui/test/spec/services/action.js
@@ -25,7 +25,6 @@ describe('Service: action', function () {
     expect(!!Action).toBe(true);
 
     var set = new Action.Set();
-
     var pkt = new Packet.Packet('test');
 
     set.setField(new Action.SetField(
@@ -33,17 +32,33 @@ describe('Service: action', function () {
       Ethernet.name, Ethernet.src,
       Ethernet.mkMAC('01:03:05:07:09:0b')));
     
+    set.step(null, {
+      packet: pkt
+    });
+    
+    expect(pkt.protocols[0].src().toString()).toBe('01:03:05:07:09:0b');
+    expect(pkt.protocols[0].dst().toString()).toBe('00:00:00:00:00:00');
+    expect(pkt.protocols[0].type().toString(16)).toBe('0x0000');
+    
     set.setField(new Action.SetField(
       null, 
       Ethernet.name, Ethernet.dst,
       Ethernet.mkMAC('ff:ff:ff:ff:ff:ff')));
+    
+    set.step(null, {
+      packet: pkt
+    });
+    
+    expect(pkt.protocols[0].src().toString()).toBe('01:03:05:07:09:0b');
+    expect(pkt.protocols[0].dst().toString()).toBe('ff:ff:ff:ff:ff:ff');
+    expect(pkt.protocols[0].type().toString(16)).toBe('0x0000');
     
     set.setField(new Action.SetField(
       null, 
       Ethernet.name, Ethernet.type,
       Ethernet.mkType('0x0806')));
 
-    set.execute(null, {
+    set.step(null, {
       packet: pkt
     });
 
