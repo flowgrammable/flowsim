a2494d910f4104008e1314b5f92269578609bc6f
Jasson Casey <jasson@flowgrammable.com>
2014-11-10 12:53:55 -0500

    latest

---
+++ spike/extractions.js
@@ -0,0 +1,32 @@
+
+var v0 = {
+  base: {
+    name: 'Internal',
+    attrs: [{
+      name: 'in_port',
+      value: 4,
+    }, {
+      name: 'in_phy_port',
+      value: 6,
+    }, {
+      name: 'tunnel',
+      value: '0xffffffffffffffff'
+    }, {
+      name: 'tunnel',
+      value: 12435
+    }]
+  }
+  protocols: [{
+    name: 'Ethernet',
+    attrs: [{
+      name: 'Src',
+      value: '00:02:03:04:05:06' 
+    }, {
+      name: 'Dst',
+      value: '00:20:30:40:50:60'
+    }, {
+      name: 'Type',
+      value: 'VLAN (0x8100'
+    }]
+  }]
+};
