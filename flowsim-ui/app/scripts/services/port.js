e284644c2ddadadde4da8e30986a1d0c88b43e5d
Colton Chojnacki <coltonchojnacki@gmail.com>
2014-11-11 10:20:37 -0600

    port update

--- flowsim-ui/app/scripts/services/port.js
+++ flowsim-ui/app/scripts/services/port.js
@@ -11,19 +11,34 @@ angular.module('flowsimUiApp')
   .service('Port', function(UInt) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
+var ReservedPorts = {
+  'InPort'      : 0xfffffff8,
+  'Table'       : 0xfffffff9,
+  'Normal'      : 0xfffffffa,
+  'Flood'       : 0xfffffffb,
+  'All'         : 0xfffffffc,
+  'Controller'  : 0xfffffffd,
+  'Local'       : 0xfffffffe,
+  'Any'         : 0xffffffff
+};
+
 function Port(port, id, phy){
   if(_.isObject(port)){
     this._id = new UInt.UInt(port._id);
     this._phy = new UInt.UInt(port._phy);
   } else {
     this._id = new UInt.UInt(null, id, 4);
-    this._phy = new UInt.UInt(null, id, 4);
+    this._phy = new UInt.UInt(null, phy, 4);
   }
 }
 
 Port.prototype.id = function(id){
   if(id){
-    this._id = new UInt.UInt(id);
+    if(id instanceof UInt.UInt){
+      this._id = new UInt.UInt(id);
+    } else {
+      this._id = new UInt.UInt(null, id, 4);
+    }
   } else {
     return this._id;
   }
@@ -31,7 +46,11 @@ Port.prototype.id = function(id){
 
 Port.prototype.phy = function(phy){
   if(phy){
-    this._phy = new UInt.UInt(phy);
+    if(phy instanceof UInt.UInt){
+      this._phy = new UInt.UInt(phy);
+    } else {
+      this._phy = new UInt.UInt(phy);
+    }
   } else {
     return this._phy;
   }
@@ -42,8 +61,26 @@ Port.prototype.clone = function(){
 };
 
 Port.prototype.toString = function() {
-  return 'id: '+ this._id.toString(16);
+  return 'id: '+ this._id.toString(16) +'\n'+
+         'phy:'+ this._phy.toString(16);
 };
+
+Port.Match = function(match, id){
+  if(_.isObject(match)){
+    this._match = new UInt.Match(match._match);
+  } else {
+    this._match = new UInt.mkExact(id);
+  }
+};
+
+Port.Match.prototype.match = function(id){
+    return this._match.match(id._id);
+};
+
+function mkInportMatch(id){
+  return new Port.Match(null, id);
+}
+
 
 var TIPS = {
   id: 'Logical identifier',
@@ -61,7 +98,10 @@ function mkPort(id, phy){
 
 return {
   Port: Port,
-  mkPort: mkPort
+  mkPort: mkPort,
+  Inport: Port,
+  mkInportMatch: mkInportMatch,
+  mkPhyportMatch: mkInportMatch
 };
 
 });
--- flowsim-ui/test/spec/services/port.js
+++ flowsim-ui/test/spec/services/port.js
@@ -17,6 +17,29 @@ describe('Service: port', function () {
 
   it('port constructor pass', function() {
     new Port.mkPort();
+    new Port.mkPort('0x000000001', '0x000000001');
+  });
+
+  it('Inport match pass', function(){
+    var inport  = Port.mkPort('0x00000001','0x00000001');
+    var inportMatch = Port.mkInportMatch(inport.id());
+
+    var p  = Port.mkPort('0x00000001', '0xabcdeff1');
+    var p2 = Port.mkPort('0x00000008', '0xddeeffaa');
+
+    expect(inportMatch.match(p)).toBe(true);
+    expect(inportMatch.match(p2)).toBe(false);
+  });
+
+  it('Phyport match pass', function(){
+    var port  = Port.mkPort('0x00000001','0xabcdeff1');
+    var phyportMatch = Port.mkPhyportMatch(port.phy());
+
+    var p  = Port.mkPort('0x00000001', '0xabcdeff1');
+    var p2 = Port.mkPort('0x00000008', '0xddeeffaa');
+    console.log('p: ', p._phy);
+    expect(phyportMatch.match(p)).toBe(true);
+    expect(phyportMatch.match(p2)).toBe(false);
   });
 
 });
