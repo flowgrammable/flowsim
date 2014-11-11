f3eb0866377d2a4e1144fbd938e81e0e6b886dcf
Muxi Yan <ximuyan@gmail.com>
2014-11-11 17:11:06 -0600

    tcp.js test

--- flowsim-ui/test/spec/services/tcp.js
+++ flowsim-ui/test/spec/services/tcp.js
@@ -11,8 +11,24 @@ describe('Service: TCP', function () {
     TCP = _TCP_;
   }));
 
-  it('should do something', function () {
+  it('Construction Pass', function () {
     expect(!!TCP).toBe(true);
-  });
 
+    new TCP.TCP();
+    new TCP.TCP_UI();
+    new TCP.TCP_UI(new TCP());
+  }
+
+  it('stringify Pass', function () {
+    expect(!!TCP).toBe(true);
+
+    var tcp1 = new TCP.TCP();
+    tcp1.fields.src = 22;
+    tcp1.fields.dst = 2222;
+    var tcp2 = JSON.stringify(tcp1);
+    var tcp3 = new TCP.TCP(JSON.parse(tcp2));
+    
+    expect(tcp3.field.src).toBe(22);
+    expect(tcp3.field.dst).toBe(2222);
+  }
 });
