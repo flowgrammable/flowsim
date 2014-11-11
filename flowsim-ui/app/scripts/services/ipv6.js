1d22071410812cce742481cc8c198d4d90b7e413
Jasson Casey <jasson@flowgrammable.com>
2014-11-11 08:05:41 -0500

    Resolved jshint issues on app/scripts/services/ipv6.js

--- flowsim-ui/app/scripts/services/ipv6.js
+++ flowsim-ui/app/scripts/services/ipv6.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('flowsimUiApp')
-  .service('IPV6', function IPV6(fgConstraints, fgUI){
+  .service('IPV6', function(fgConstraints, fgUI){
 
 var NAME = 'IPv6';
 
-var Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
-[0-9a-fA-F]{1,4}
+
+var fullyQualifiedPattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
 
 // Javascript regex patterns that support prefix/infix/postfix :: notation
 // prefix/postfix are complete
@@ -30,7 +30,7 @@ var Payloads = {
   'Payload' : 0
 };
 
-function _IPV6() {
+function IPV6() {
   this.name = NAME;
   this.bytes = 40;
   this.fields = {
@@ -40,8 +40,8 @@ function _IPV6() {
   };
 }
 
-function _IPV6_UI(ipv6) {
-  ipv6 = ipv6 === undefined ? new _IPV6() : ipv6;
+function IPV6_UI(ipv6) {
+  ipv6 = ipv6 === undefined ? new IPV6() : ipv6;
   this.name = NAME;
   this.bytes = ipv6.bytes;
   this.attrs = _.map(ipv6.fields, function(value, key) {
@@ -78,29 +78,29 @@ function _IPV6_UI(ipv6) {
   });
 }
 
-_IPV6_UI.prototype.toBase = function() {
-  var result = new _IPV6();
+IPV6_UI.prototype.toBase = function() {
+  var result = new IPV6();
   result.name = this.name;
-  result.bytes = this.bytes
+  result.bytes = this.bytes;
   result.fields = fgUI.stripLabelInputs(this.attrs);
   return result;
 };
 
-_IPV6_UI.prototype.setPayload = function(name) {
+IPV6_UI.prototype.setPayload = function(name) {
   this.attrs[2].value = Payloads[name] || 0;
 };
 
-_IPV6_UI.prototype.clearPayload = function(name) {
+IPV6_UI.prototype.clearPayload = function(name) {
   this.attrs[2].value = 0;
 };
 
 this.name = NAME;
 this.create = function() {
-  return new _IPV6();
+  return new IPV6();
 };
 
 this.createUI = function(ipv6) {
-  return new _IPV6_UI(ipv6);
+  return new IPV6_UI(ipv6);
 };
 
 this.Payloads = Object.keys(Payloads);
