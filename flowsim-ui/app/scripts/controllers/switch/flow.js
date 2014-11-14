0821c1e6c568ede375ca69b89dab07ddd89baa5e
Jasson Casey <jasson@flowgrammable.com>
2014-11-14 00:33:30 -0500

    Adding flow dialog

---
+++ flowsim-ui/app/scripts/controllers/switch/flow.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowsimUiApp.controller:SwitchFlowCtrl
+ * @description
+ * # SwitchFlowCtrl
+ * Controller of the flowsimUiApp
+ */
+angular.module('flowsimUiApp')
+  .controller('SwitchFlowCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- flowsim-ui/app/scripts/controllers/switch/table.js
+++ flowsim-ui/app/scripts/controllers/switch/table.js
@@ -8,11 +8,25 @@
  * Controller of the flowsimUiApp
  */
 angular.module('flowsimUiApp')
-  .controller('SwitchTableCtrl', function ($scope) {
+  .controller('SwitchTableCtrl', function ($scope, $modal) {
 
   $scope.active = 0;
   $scope.toggle = function(idx) {
     $scope.active = idx;
+  };
+
+  $scope.openFlow = function(idx) {
+    $modal.open({
+      templateUrl: 'views/dialog/switch/flow.html',
+      controller: 'DialogSwitchFlowCtrl',
+      size: 'lg',
+      resolve: {
+        flow: function() { 
+          return $scope.device.tables.tables.get(priority, idx); 
+        }
+      }
+    }).result.then(function (flow) {
+    });
   };
 
 });
