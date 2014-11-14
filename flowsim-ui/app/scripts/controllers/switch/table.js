0eb5782767cb5697c4804d281e7ce2e7c9c4ab2e
Jasson Casey <jasson@flowgrammable.com>
2014-11-14 00:03:37 -0500

    latest

---
+++ flowsim-ui/app/scripts/controllers/switch/table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flowsimUiApp.controller:SwitchTableCtrl
+ * @description
+ * # SwitchTableCtrl
+ * Controller of the flowsimUiApp
+ */
+angular.module('flowsimUiApp')
+  .controller('SwitchTableCtrl', function ($scope) {
+
+  $scope.active = 0;
+  $scope.toggle = function(idx) {
+    $scope.active = idx;
+  };
+
+});
