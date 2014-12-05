291e4b664563e30630b3b8eae71787a83d086413
Colton Chojnacki <coltonchojnacki@gmail.com>
2014-12-05 17:40:58 -0600

    vlan init

---
+++ flowsim-ui/app/scripts/services/arp2.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('flowsimUiApp')
+  .factory('ARP2', function arp2() {
+
+var ARP = {
+  name: 'ARP',
+  shortName: 'arp',
+  bytes: 28,
+  fields: [{
+    
+  }]
+}
+
+});
