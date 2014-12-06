361fbdf1e7ef0bbc79a0b0ac9bef74d4da7d4e10
Jasson Casey <jasson@flowgrammable.com>
2014-12-06 09:12:20 -0500

    Deleting dead code

--- flowsim-ui/app/scripts/controllers/dialog/profile/instruction.js
+++ flowsim-ui/app/scripts/controllers/dialog/profile/instruction.js
@@ -74,59 +74,12 @@ angular.module('flowsimUiApp')
       metadataMaskableBits: instruction.metadata,
       metadataMaskableBitsTip: 'Determine which metadata bits can be masked',
       metadataMaskableBitsTest: UInt.is(64),
-      gotoValue: '',
-      gotoValues: []
     };
 
     // Organize the action profiles in cels of like-protocol
     // and rows of 3 columns
     $scope.applyRows = orderProfiles(instruction.apply.profiles);
     $scope.writeRows = orderProfiles(instruction.write.profiles);
-
-    /*
-    $scope.goto_ = {
-      nextTables : ''
-    };
-    $scope.errorMsg = '';
-
-    $scope.add = function() {
-      var extract = /^([0-9]+)(\.\.([0-9]+))?$/;
-      var match, result;
-    
-      $scope.errorMsg = '';
-
-      if(!$scope.tests.goto_($scope.goto_.nextTables)) {
-        $scope.errorMsg = 'Invalid Range';
-      } else {
-        match = $scope.goto_.nextTables.match(extract);
-        result = {};
-        if(match[1] && fgConstraints.isUInt(0, 255)(match[1])) {
-          result.first = match[1];
-        } else {
-          $scope.errorMsg = 'Invalid Range';
-        }
-        if(match[3]) {
-          if(fgConstraints.isUInt(0, 255)(match[3]) && 
-             parseInt(match[1]) <= parseInt(match[3])) {
-            result.second = match[3];
-            $scope.instruction.goto_.push(result);
-            $scope.goto_.nextTables = '';
-          }
-          else {
-            $scope.errorMsg = 'Invalid Range';
-          }
-        } else {
-          result.second = result.first;
-          $scope.instruction.goto_.push(result);
-          $scope.goto_.nextTables = '';
-        }
-      }
-    };
-
-    $scope.del = function(idx) {
-      $scope.instruction.goto_.splice(idx, 1);
-    };
-    */
 
     $scope.ok = function() {
       // Copy the enablements
