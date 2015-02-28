40e653510c2d4374b4680fd909c1bab90238a604
sashajo <sashajo@github.com>
2015-02-28 14:19:52 -0500

    docker readme edits

--- docker/run-node.sh
+++ docker/run-node.sh
@@ -4,4 +4,4 @@ POSTGRES_NAME=flowsim-dev-postgres
 PORT=8080
 TLS_PORT=8081
 set -x
-docker run --name $NODE_NAME --link $POSTGRES_NAME:postgres -d -p $PORT:8080 -p $TLS_PORT:8081 flowgrammable/flowsim-server
+docker run --name $NODE_NAME --restart="always" --link $POSTGRES_NAME:postgres -d -p $PORT:8080 -p $TLS_PORT:8081 flowgrammable/flowsim-node
